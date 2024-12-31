14bd3836f10d815be7198799642b8b2371bc1405
Seo-Yurim <yurim2204@naver.com>
2024-12-31 18:42:07 +0900

    docs: 프리티어 및 기타 설정 파일 세팅

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@/styles/globals.tailwind.css";
+
+const pretendard = localFont({
+  src: "../../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // 보통 SSR에서는 staleTime을 0 이상으로 해줌으로써
+            // 클라이언트 사이드에서 바로 다시 데이터를 refetch 하는 것을 피한다.
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className={pretendard.className}>
+        <Component {...pageProps} />
+      </div>
+    </QueryClientProvider>
+  );
 }
