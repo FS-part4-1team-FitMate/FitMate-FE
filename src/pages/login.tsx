9806998b72a883c9d5ba14039c3709e59f7c65b3
kipid <kipacti@gmail.com>
2025-01-02 17:16:23 +0900

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,20 @@
+import PopUp from "@/components/PopUp";
+import { useSetUser } from "@/contexts/UserProvider";
 import { postLogin } from "@/lib/api/authService";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
-const EMAIL_REGEX = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+export const EMAIL_REGEX = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
 
 function LogIn() {
+  const setUser = useSetUser();
+  const [error, setError] = useState<null | Error | {
+    message: string;
+    onOK?: () => void;
+    onCancel?: () => void;
+  }>(null);
   const [pwdIsVisible, setPwdIsVisible] = useState(false);
   const {
     register,
@@ -19,21 +28,21 @@ function LogIn() {
     }
   });
 
-	useEffect(() => {
-		console.log(errors);
-	}, [errors]);
-
   const onSubmit = async (data: { email: string; password: string }) => {
     try {
       const userData = await postLogin(data);
+      if ('user' in userData) {
+        setUser(userData.user);
+      }
+      localStorage.setItem('userData', JSON.stringify(userData));
     } catch (err) {
-
+      setError({ message: (err as Error).message });
     }
   };
 
   return (
-    <div className="flex flex-col justify-center items-center gap-[32px] h-screen w-[384px] max-w-full mx-auto p-[4px]">
-      <Image src="/assets/img/logo_xl.svg" alt="Logo" width={384} height={124} />
+    <main className="flex flex-col justify-center items-center gap-[32px] h-screen w-[384px] max-w-full mx-auto p-[4px]">
+      <Image src="/assets/img/logo_xl.svg" alt="Logo" width={384} height={124} priority />
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-[16px] items-stretch w-full">
         <label className="w-full text-lg" htmlFor="email">이메일</label>
         <input className="w-full text-lg p-[8px] h-[40px] text-slate-700 border border-gray-300 rounded-2xl"
@@ -63,23 +72,24 @@ function LogIn() {
             id="password"
             placeholder="비밀번호"
           />
-					<Image className="absolute right-[8px] top-[8px] bottom-[8px]" width={24} height={24} src={pwdIsVisible ? "/assets/ic/ic_visibility_on.svg" : "/assets/ic/ic_visibility_off.svg"} alt="eye" onClick={() => setPwdIsVisible(!pwdIsVisible)} />
+          <Image className="absolute right-[8px] top-[8px] bottom-[8px]" width={24} height={24} src={pwdIsVisible ? "/assets/ic/ic_visibility_on.svg" : "/assets/ic/ic_visibility_off.svg"} alt="eye" onClick={() => setPwdIsVisible(!pwdIsVisible)} />
         </div>
         {errors.password && <p className="text-red-400 text-sm">{errors.password.message}</p>}
-        <button className="w-full h-[40px] text-lg rounded-2xl text-white bg-blue-600" type="submit">로그인</button>
+        <button className="w-full h-[40px] text-lg rounded-2xl text-white bg-blue-600 disabled:bg-slate-600" type="submit" disabled={!!errors.email || !!errors.password}>로그인</button>
       </form>
-			<div className="flex flex-col text-lg justify-center items-center gap-[8px]">
-				<div>SNS 계정으로 로그인</div>
-				<div className="flex justify-center items-center gap-[8px]">
-					<Image src="/assets/ic/ic_google_sm.svg" alt="google" width={40} height={40} />
-					<Image src="/assets/ic/ic_kakao_sm.svg" alt="kakao" width={40} height={40} />
-					<Image src="/assets/ic/ic_naver_sm.svg" alt="naver" width={40} height={40} />
-				</div>
-			</div>
-			<div>
-				아직 핏메이트 회원이 아니신가요? <a href="/signup" className="text-blue-600">일반 회원가입</a>, <a href="/signup" className="text-blue-600">트레이너 회원가입</a>
-			</div>
-    </div>
+      <div className="flex flex-col text-lg justify-center items-center gap-[8px]">
+        <div>SNS 계정으로 로그인</div>
+        <div className="flex justify-center items-center gap-[8px]">
+          <Image src="/assets/ic/ic_google_sm.svg" alt="google" width={40} height={40} />
+          <Image src="/assets/ic/ic_kakao_sm.svg" alt="kakao" width={40} height={40} />
+          <Image src="/assets/ic/ic_naver_sm.svg" alt="naver" width={40} height={40} />
+        </div>
+      </div>
+      <div className="text-lg">
+        아직 핏메이트 회원이 아니신가요? <Link href="/user/signup" className="text-blue-600">일반 회원가입</Link>, <Link href="/trainer/signup" className="text-blue-600">강사님 회원가입</Link>
+      </div>
+      <PopUp error={error} setError={setError} />
+    </main>
   );
 }
 
