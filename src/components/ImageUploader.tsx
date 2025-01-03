c8db1b891c0ac99d73902c3f53ae5c6bd8df39f8
kipid <kipacti@gmail.com>
2025-01-03 16:43:23 +0900

    Feat: components/ImageUploader.tsx

---
+++ src/components/ImageUploader.tsx
@@ -0,0 +1,33 @@
+import { ic_profile_default_sm } from "@/imageExports";
+import React, { useState, ChangeEvent } from "react";
+
+const ImageUploader: React.FC = () => {
+  const [imageSrc, setImageSrc] = useState<string>(ic_profile_default_sm.src);
+
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        if (e.target?.result) {
+          setImageSrc(e.target.result as string);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div>
+      <img
+        src={imageSrc}
+        alt="Profile Preview"
+        className="w-[150px] h-[150px] rounded-3xl border-2 border-gray-300 object-cover mb-[10px]"
+      />
+      <br />
+      <input id="profileImage" name="profileImage" className="p-0 m-0 text-sm rounded-none" type="file" accept="image/*" onChange={handleImageChange} />
+    </div>
+  );
+};
+
+export default ImageUploader;
