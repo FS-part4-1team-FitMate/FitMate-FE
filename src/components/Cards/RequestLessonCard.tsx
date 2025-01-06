dce2eb3e5bde492a00d5a9c6cbe5c135143563f9
Seo-Yurim <yurim2204@naver.com>
2025-01-06 14:48:20 +0900

    design: 받은 요청 카드 템플릿 구현

---
+++ src/components/Cards/RequestLessonCard.tsx
@@ -0,0 +1,33 @@
+import CardContainer from "../Common/Card/CardContainer";
+import VerticalLine from "../Common/Card/VerticalLine";
+
+export default function RequestLessonCard() {
+  return (
+    <CardContainer width="95.5rem" gap="2.4rem">
+      <div className="flex justify-between">
+        <p className="text-xl font-semibold">칩 넣을 자리</p>
+        <p className="text-gray-500 text-xs font-normal">1시간 전</p>
+      </div>
+      <p className="text-xl font-semibold">김고객 고객님</p>
+      <div className="flex items-center gap-[1.6rem]">
+        <p className="text-2lg font-medium">레슨 시작일: 2025.02.02</p>
+        <VerticalLine height="1.6rem" />
+        <p className="text-2lg font-medium">레슨 종료일: 2025.02.15</p>
+        <VerticalLine height="1.6rem" />
+        <p className="text-2lg font-medium">레슨 장소: 온라인</p>
+      </div>
+      <div className="flex justify-end items-center gap-[1.6rem]">
+        <p className="text-2lg font-medium">견적 금액</p>
+        <p className="text-2xl font-bold">180,000원</p>
+      </div>
+      <div className="flex gap-[1.1rem]">
+        <button className="flex-1 h-[6.4rem] p-[1.6rem] rounded-[1.6rem] text-gray-50 text-xl font-semibold bg-blue-300">
+          견적 보내기
+        </button>
+        <button className="flex-1 h-[6.4rem] p-[1.6rem] border border-blue-300 rounded-[1.6rem] text-blue-300 text-xl font-semibold bg-gray-50">
+          반려
+        </button>
+      </div>
+    </CardContainer>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import FindTrainerCard from "@/components/Cards/FindTrainerCard";
 import PendingLessonCard from "@/components/Cards/PendingLessonCard";
 import QuoteCard from "@/components/Cards/QuoteCard";
+import RequestLessonCard from "@/components/Cards/RequestLessonCard";
 
 export default function Home() {
   return (
-    <div className=" bg-pink-200 m-10 p-10">
+    <div className="flex flex-col gap-4 bg-pink-200 m-10 p-10">
       <FindTrainerCard />
+      <RequestLessonCard />
       <PendingLessonCard />
       <QuoteCard />
     </div>
