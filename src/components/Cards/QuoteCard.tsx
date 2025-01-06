49315fa7e47517c77953a437c9a044be7717d412
Seo-Yurim <yurim2204@naver.com>
2025-01-06 14:38:08 +0900

    design: 견적 내역 카드 템플릿 구현

---
+++ src/components/Cards/QuoteCard.tsx
@@ -0,0 +1,16 @@
+import CardContainer from "../Common/Card/CardContainer";
+import TrainerInfo from "../Common/Card/TrainerInfo";
+
+export default function QuoteCard() {
+  return (
+    <CardContainer width="68.8rem" gap="1.6rem">
+      <div className="text-xl font-semibold">칩 넣을 자리</div>
+      <p className="text-2xl font-semibold">고객님에게 맞춤형 레슨을 해드립니다.</p>
+      <TrainerInfo />
+      <div className="flex justify-end items-center gap-[1.6rem]">
+        <p className="text-2lg font-medium">견적 금액</p>
+        <p className="text-2xl font-bold">180,000원</p>
+      </div>
+    </CardContainer>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import FindTrainerCard from "@/components/Cards/FindTrainerCard";
 import PendingLessonCard from "@/components/Cards/PendingLessonCard";
+import QuoteCard from "@/components/Cards/QuoteCard";
 
 export default function Home() {
   return (
     <div className=" bg-pink-200 m-10 p-10">
       <FindTrainerCard />
       <PendingLessonCard />
+      <QuoteCard />
     </div>
   );
 }
