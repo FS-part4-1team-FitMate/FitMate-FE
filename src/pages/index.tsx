63c174919f3eb4d032b06535f7f7210d79a79aa8
Seo-Yurim <yurim2204@naver.com>
2025-01-06 16:07:00 +0900

    test: 테스트 코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,7 @@
-import FindTrainerCard from "@/components/Cards/FindTrainerCard";
-import PendingLessonCard from "@/components/Cards/PendingLessonCard";
-import QuoteCard from "@/components/Cards/QuoteCard";
-import RequestLessonCard from "@/components/Cards/RequestLessonCard";
-
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4 bg-pink-200 m-10 p-10">
-      <FindTrainerCard />
-      <RequestLessonCard />
-      <PendingLessonCard />
-      <QuoteCard />
+    <div className="flex bg-pink-200 m-10 p-10">
+      <p>랜딩 페이지</p>
     </div>
   );
 }
