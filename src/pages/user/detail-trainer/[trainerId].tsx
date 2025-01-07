6d8c28aae2cb6ec51a67c5fb819c2f2b9860e28d
Seo-Yurim <yurim2204@naver.com>
2025-01-07 16:43:42 +0900

    design: 수평선 너비 동적으로 받아오게 수정 및 css 정리

--- src/pages/user/detail-trainer/[trainerId].tsx
+++ src/pages/user/detail-trainer/[trainerId].tsx
@@ -2,31 +2,36 @@ import FindTrainerCard from "@/components/Cards/FindTrainerCard";
 import { HorizontalLine } from "@/components/Common/Card/Line";
 import ShareSNS from "@/components/Common/ShareSNS";
 
+const area_wrap = "flex flex-col gap-16";
+const content_wrap = "flex flex-col gap-[3.2rem]";
+const label = "text-2xl font-bold";
+const content = "text-2lg font-normal";
+
 export default function DetailTrainer() {
   return (
     <div className="flex justify-between max-w-[144rem] m-auto mt-[5.6rem]">
-      <div className="flex flex-col gap-16">
+      <div className={area_wrap}>
         <FindTrainerCard item="" />
-        <HorizontalLine />
-        <div className="flex flex-col gap-[3.2rem]">
-          <h1 className="text-2xl font-bold">상세설명</h1>
-          <p className="text-2lg font-normal">description</p>
+        <HorizontalLine width="100%" />
+        <div className={content_wrap}>
+          <h1 className={label}>상세설명</h1>
+          <p className={content}>description</p>
         </div>
-        <HorizontalLine />
-        <div className="flex flex-col gap-[3.2rem]">
-          <h1 className="text-2xl font-bold">제공 서비스</h1>
-          <p className="text-2lg font-normal">스포츠</p>
+        <HorizontalLine width="100%" />
+        <div className={content_wrap}>
+          <h1 className={label}>제공 서비스</h1>
+          <p className={content}>스포츠</p>
         </div>
-        <HorizontalLine />
-        <div className="flex flex-col gap-[3.2rem]">
-          <h1 className="text-2xl font-bold">서비스 가능 지역</h1>
-          <p className="text-2lg font-normal">서울</p>
+        <HorizontalLine width="100%" />
+        <div className={content_wrap}>
+          <h1 className={label}>서비스 가능 지역</h1>
+          <p className={content}>서울</p>
         </div>
-        <HorizontalLine />
-        <div className="text-2xl font-bold">평점 및 리뷰 들어갈 부분</div>
+        <HorizontalLine width="100%" />
+        <div className={label}>평점 및 리뷰 들어갈 부분</div>
       </div>
-      <div className="flex flex-col gap-16">
-        <div className="flex flex-col gap-[3.2rem]">
+      <div className={area_wrap}>
+        <div className={content_wrap}>
           <h1 className="text-xl font-semibold">김코드 강사님에게 지정 견적을 요청해보세요</h1>
           <button className="flex justify-center items-center w-[35.3rem] h-[5.4rem] p-4 border border-line-200 rounded-[1.6rem] text-xl font-semibold bg-gray-50">
             기사님 찜하기
@@ -35,6 +40,7 @@ export default function DetailTrainer() {
             지정 견적 요청하기
           </button>
         </div>
+        <HorizontalLine width="32.7rem" />
         <ShareSNS label="나만 알기엔 아쉬운 강사님인가요?" />
       </div>
     </div>
