016c604ab1d3871eb069ead10b4f2cb5d4bc14b4
Seo-Yurim <yurim2204@naver.com>
2025-01-07 16:32:39 +0900

    design: css 수정

--- src/pages/user/detail-trainer/[trainerId].tsx
+++ src/pages/user/detail-trainer/[trainerId].tsx
@@ -1,43 +1,41 @@
+import FindTrainerCard from "@/components/Cards/FindTrainerCard";
 import { HorizontalLine } from "@/components/Common/Card/Line";
-import TrainerInfo from "@/components/Common/Card/TrainerInfo/TrainerInfo";
+import ShareSNS from "@/components/Common/ShareSNS";
 
 export default function DetailTrainer() {
   return (
-    <div className="flex justify-between mt-[5.6rem] ml-[26rem] mr-[23.4rem]">
+    <div className="flex justify-between max-w-[144rem] m-auto mt-[5.6rem]">
       <div className="flex flex-col gap-16">
-        <TrainerInfo
-          rating={3}
-          reviewCount={24}
-          experience={4}
-          lessonCount={54}
-          isFavorited={true}
-          favoriteCount={12}
-        />
+        <FindTrainerCard item="" />
         <HorizontalLine />
-        <div>
-          <h1>상세설명</h1>
-          <p>description</p>
+        <div className="flex flex-col gap-[3.2rem]">
+          <h1 className="text-2xl font-bold">상세설명</h1>
+          <p className="text-2lg font-normal">description</p>
         </div>
         <HorizontalLine />
-        <div>
-          <h1>제공 서비스</h1>
-          <p>스포츠</p>
+        <div className="flex flex-col gap-[3.2rem]">
+          <h1 className="text-2xl font-bold">제공 서비스</h1>
+          <p className="text-2lg font-normal">스포츠</p>
         </div>
         <HorizontalLine />
-        <div>
-          <h1>서비스 가능 지역</h1>
-          <p>서울</p>
-          <p>경기</p>
+        <div className="flex flex-col gap-[3.2rem]">
+          <h1 className="text-2xl font-bold">서비스 가능 지역</h1>
+          <p className="text-2lg font-normal">서울</p>
         </div>
         <HorizontalLine />
-        <div>평점 및 리뷰 들어갈 부분</div>
+        <div className="text-2xl font-bold">평점 및 리뷰 들어갈 부분</div>
       </div>
-      <div>
-        <div>
-          <h1>김코드 강사님에게 지정 견적을 요청해보세요</h1>
-          <button>기사님 찜하기</button>
-          <button>지정 견적 요청하기</button>
+      <div className="flex flex-col gap-16">
+        <div className="flex flex-col gap-[3.2rem]">
+          <h1 className="text-xl font-semibold">김코드 강사님에게 지정 견적을 요청해보세요</h1>
+          <button className="flex justify-center items-center w-[35.3rem] h-[5.4rem] p-4 border border-line-200 rounded-[1.6rem] text-xl font-semibold bg-gray-50">
+            기사님 찜하기
+          </button>
+          <button className="flex justify-center items-center w-[35.3rem] h-[5.4rem] p-[1.6rem] rounded-[1.6rem] text-gray-50 text-xl font-semibold bg-blue-300">
+            지정 견적 요청하기
+          </button>
         </div>
+        <ShareSNS label="나만 알기엔 아쉬운 강사님인가요?" />
       </div>
     </div>
   );
