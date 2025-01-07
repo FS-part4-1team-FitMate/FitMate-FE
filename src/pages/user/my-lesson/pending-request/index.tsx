bd2184f3aeed9624d3d1e8471a2e48b2f7fc705d
Seo-Yurim <yurim2204@naver.com>
2025-01-07 17:26:01 +0900

    design: 대기중인요청 페이지 UI 구현

---
+++ src/pages/user/my-lesson/pending-request/index.tsx
@@ -0,0 +1,13 @@
+import PendingLessonCard from "@/components/Cards/PendingLessonCard";
+
+export default function PendingRequest() {
+  const items = new Array(10).fill("");
+
+  return (
+    <div className="grid grid-cols-2 gap-x-[2.4rem] gap-y-[4.8rem] max-w-[144rem] mx-auto mt-16">
+      {items.map((item, index) => (
+        <PendingLessonCard key={index} item={item} />
+      ))}
+    </div>
+  );
+}
