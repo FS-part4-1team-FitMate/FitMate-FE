81c48fafb8626e079eda7c530f19777918f4bdbb
Seo-Yurim <yurim2204@naver.com>
2025-01-08 14:38:19 +0900

    feat: 체크박스 필터 컴포넌트 생성

---
+++ src/components/Filter/CheckboxFilter.tsx
@@ -0,0 +1,82 @@
+import { ic_square_check_active_md, ic_square_check_inactive_md } from "@/imageExports";
+import Image from "next/image";
+import { useState } from "react";
+
+interface CheckboxFilterProps {
+  label: string;
+  options?: string[];
+}
+
+export default function CheckboxFilter({ label, options = [] }: CheckboxFilterProps) {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [isCheckedFilter, setIsCheckedFilter] = useState<boolean[]>(
+    new Array(options.length).fill(false),
+  );
+
+  const handleCheckboxClick = (index: number) => {
+    const updatedCheckedItems = [...isCheckedFilter];
+    updatedCheckedItems[index] = !updatedCheckedItems[index];
+    setIsCheckedFilter(updatedCheckedItems);
+  };
+
+  const handleSelectAll = () => {
+    const allChecked = isCheckedFilter.every((item) => item);
+    setIsCheckedFilter(new Array(options.length).fill(!allChecked));
+  };
+
+  if (label === "지정 견적 요청") {
+    return (
+      <div className="flex justify-between items-center p-[1.6rem]">
+        <p className="text-xl font-medium">지정 견적 요청 (10)</p>
+        <Image
+          className="cursor-pointer"
+          src={isChecked ? ic_square_check_active_md : ic_square_check_inactive_md}
+          width={36}
+          height={36}
+          onClick={() => setIsChecked((prev) => !prev)}
+          alt="체크박스"
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-col gap-[2.4rem]">
+        <div className="flex justify-between w-[32.8rem] py-[1.6rem] px-4 border-b border-line-200">
+          <h1 className="text-xl font-medium">{label}</h1>
+          <div className="flex items-center">
+            <Image
+              className="cursor-pointer"
+              src={
+                isCheckedFilter.every((item) => item)
+                  ? ic_square_check_active_md
+                  : ic_square_check_inactive_md
+              }
+              width={36}
+              height={36}
+              onClick={handleSelectAll}
+              alt="체크박스"
+            />
+            <p className="text-gray-300 text-2lg font-normal">전체선택</p>
+          </div>
+        </div>
+        <div>
+          {options.map((_, index) => (
+            <div className="flex justify-between items-center p-[1.6rem] border-b border-line-100">
+              <p className="text-2lg font-medium">{options[index]} (10)</p>
+              <Image
+                className="cursor-pointer"
+                src={
+                  isCheckedFilter[index] ? ic_square_check_active_md : ic_square_check_inactive_md
+                }
+                width={36}
+                height={36}
+                onClick={() => handleCheckboxClick(index)}
+                alt="체크박스"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
--- src/pages/trainer/received-request.tsx
+++ src/pages/trainer/received-request.tsx
@@ -1,17 +1,22 @@
+import { ic_square_check_inactive_md } from "@/imageExports";
+import Image from "next/image";
 import { useState } from "react";
-import { UserSort } from "@/types/dropdown";
+import { GenderFilter, ServiceFilter, UserSort } from "@/types/dropdown";
 import RequestLessonCard from "@/components/Cards/RequestLessonCard";
-import { HorizontalLine } from "@/components/Common/Line";
 import Search from "@/components/Common/Search";
 import Title from "@/components/Common/Title";
 import Dropdown from "@/components/Dropdown/Dropdown";
+import CheckboxFilter from "@/components/Filter/CheckboxFilter";
 
 const data = [{ name: "김코드" }, { name: "강코드" }, { name: "박코드" }];
 
 export default function ReceivedRequest() {
   // 오류 방지용 임시 trainerID 지정
   const [keyword, setKeyword] = useState<string>("");
+
   const userSort: UserSort[] = ["레슨 빠른 순", "레슨 느린 순", "최근 요청 순"];
+  const serviceFilter: ServiceFilter[] = ["재활운동", "스포츠", "피트니스"];
+  const genderFilter: GenderFilter[] = ["남자", "여자"];
 
   // 타입 지정 필요
   const [filteredData, setFilteredData] = useState(data);
@@ -28,13 +33,10 @@ export default function ReceivedRequest() {
       <Title title="받은 요청" />
       <div className="flex justify-between max-w-[140rem] w-full mx-auto py-[2.4rem]">
         <div className="flex flex-col gap-[4.6rem]">
-          <div className="flex justify-between">
-            <p>이사 유형</p>
-            <p>전체선택</p>
-          </div>
-          <HorizontalLine width="100%" />
-          <div>
-            <p>재활운동</p>
+          <div className="flex flex-col gap-[2.4rem]">
+            <CheckboxFilter label="운동 유형" options={serviceFilter} />
+            <CheckboxFilter label="성별" options={genderFilter} />
+            <CheckboxFilter label="지정 견적 요청" />
           </div>
         </div>
         <div className="flex flex-col gap-[3.2rem]">
