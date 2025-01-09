3819447bde0818c7e73ecc3d425e65b514528e72
Seo-Yurim <yurim2204@naver.com>
2025-01-09 15:23:41 +0900

    design: 강사님 정보 컴포넌트들 반응형 구현

--- src/components/Common/Card/TrainerInfo/LessonCount.tsx
+++ src/components/Common/Card/TrainerInfo/LessonCount.tsx
@@ -1,9 +1,8 @@
-const container = "flex items-center gap-[0.6rem]";
-const text = "text-lg font-medium";
+const text = "font-medium pc:text-lg tablet:text-sm mobile:text-sm";
 
 export default function LessonCount({ lessonCount }: { lessonCount?: number }) {
   return (
-    <div className={container}>
+    <div className="flex items-center pc:gap-[0.6rem] tablet:gap-[0.4rem] mobile:gap-[0.4rem]">
       <p className={text}>{lessonCount}건</p>
       <p className={`${text} text-gray-300`}>확정</p>
     </div>
--- src/components/Common/Card/TrainerInfo/Rating.tsx
+++ src/components/Common/Card/TrainerInfo/Rating.tsx
@@ -1,8 +1,9 @@
 import { ic_star_active_md } from "@/imageExports";
 import Image from "next/image";
 
-const container = "flex items-center gap-[0.6rem]";
-const text = "text-lg font-medium";
+const container = "flex pc:gap-[0.4rem] tablet:gap-[0.2rem] mobile:gap-[0.2rem]";
+const text = "font-medium pc:text-lg tablet:text-sm mobile:text-sm";
+const star = "pc:w-[2.4rem] pc:h-[2.4rem] tablet:w-8 tablet:h-8 mobile:w-8 mobile:h-8";
 
 interface RatingProps {
   rating?: number;
@@ -12,7 +13,7 @@ interface RatingProps {
 export default function Rating({ rating, reviewCount }: RatingProps) {
   return (
     <div className={container}>
-      <Image src={ic_star_active_md} width={24} height={24} alt="별점" />
+      <Image className={star} src={ic_star_active_md} width={24} height={24} alt="별점" />
       <div className={container}>
         <p className={text}>{rating?.toFixed(1)}</p>
         <p className={`${text} text-gray-300`}>({reviewCount})</p>
