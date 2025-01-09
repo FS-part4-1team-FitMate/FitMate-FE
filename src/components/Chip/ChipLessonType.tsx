14a2dc95d4e8c55f6b2fcc9ffeb90e3f445a131e
kipid <kipacti@gmail.com>
2025-01-09 16:55:54 +0900

    Create ChipLessonType.tsx

---
+++ src/components/Chip/ChipLessonType.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { LessonType, lessonType_trans } from "@/types/types";
+
+interface Props {
+  lessonType: LessonType;
+  size: "sm" | "lg" | "xl";
+}
+
+function ChipLessonType({ lessonType, size }: Props) {
+  return (
+    <div
+      className={`inline-flex justify-center items-center text-${size} rounded-lg border border-solid border-blue-500 text-blue-500 bg-blue-100 gap-[2px]`}
+    >
+      <Image src={lessonType_trans[lessonType].img} width={24} height={24} alt="레슨 타입" />
+      {size !== "sm" && lessonType_trans[lessonType].ko}
+    </div>
+  );
+}
+
+export default ChipLessonType;
