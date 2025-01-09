36d14303aa1764058f8e9b66b98a20fbd7566904
kipid <kipacti@gmail.com>
2025-01-09 16:55:59 +0900

    Update chip-test.tsx

--- src/pages/chip-test.tsx
+++ src/pages/chip-test.tsx
@@ -1,4 +1,5 @@
-import { Region } from "@/types/types";
+import { LessonType, Region } from "@/types/types";
+import ChipLessonType from "@/components/Chip/ChipLessonType";
 import ChipRegion from "@/components/Chip/ChipRegion";
 
 function ChipTest() {
@@ -6,6 +7,15 @@ function ChipTest() {
     <>
       <ChipRegion region={Region.SEOUL} size="lg" />
       <ChipRegion region={Region.JEJU} size="xl" />
+      <ChipLessonType lessonType={LessonType.SPORTS} size="sm" />
+      <ChipLessonType lessonType={LessonType.SPORTS} size="lg" />
+      <ChipLessonType lessonType={LessonType.SPORTS} size="xl" />
+      <ChipLessonType lessonType={LessonType.FITNESS} size="sm" />
+      <ChipLessonType lessonType={LessonType.FITNESS} size="lg" />
+      <ChipLessonType lessonType={LessonType.FITNESS} size="xl" />
+      <ChipLessonType lessonType={LessonType.REHAB} size="sm" />
+      <ChipLessonType lessonType={LessonType.REHAB} size="lg" />
+      <ChipLessonType lessonType={LessonType.REHAB} size="xl" />
     </>
   );
 }
