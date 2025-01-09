055755edbe6d70e86498ae8856c597675a56341c
kipid <kipacti@gmail.com>
2025-01-09 10:33:22 +0900

    Update types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -35,6 +35,46 @@ export enum Region {
   JEJU = "JEJU",
 }
 
+export const region_trans = {
+  [Region.SEOUL]: "서울",
+  [Region.GYEONGGI]: "경기",
+  [Region.INCHEON]: "인천",
+  [Region.DAEJEON]: "대전",
+  [Region.DAEGU]: "대구",
+  [Region.ULSAN]: "울산",
+  [Region.BUSAN]: "부산",
+  [Region.GWANGJU]: "광주",
+  [Region.SEJONG]: "세종",
+  [Region.GANGWON]: "강원",
+  [Region.CHUNGBUK]: "충북",
+  [Region.CHUNGNAM]: "충남",
+  [Region.JEONBUK]: "전북",
+  [Region.JEONNAM]: "전남",
+  [Region.GYEONGBUK]: "경북",
+  [Region.GYEONGNAM]: "경남",
+  [Region.JEJU]: "제주",
+};
+
+export const region_options = [
+  { name: "서울", value: Region.SEOUL },
+  { name: "경기", value: Region.GYEONGGI },
+  { name: "인천", value: Region.INCHEON },
+  { name: "대전", value: Region.DAEJEON },
+  { name: "대구", value: Region.DAEGU },
+  { name: "울산", value: Region.ULSAN },
+  { name: "부산", value: Region.BUSAN },
+  { name: "광주", value: Region.GWANGJU },
+  { name: "세종", value: Region.SEJONG },
+  { name: "강원", value: Region.GANGWON },
+  { name: "충북", value: Region.CHUNGBUK },
+  { name: "충남", value: Region.CHUNGNAM },
+  { name: "전북", value: Region.JEONBUK },
+  { name: "전남", value: Region.JEONNAM },
+  { name: "경북", value: Region.GYEONGBUK },
+  { name: "경남", value: Region.GYEONGNAM },
+  { name: "제주", value: Region.JEJU },
+];
+
 export enum LocationType {
   ONLINE = "ONLINE",
   OFFLINE = "OFFLINE",
