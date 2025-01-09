24f54317bbe016885986da91e103c4c270863fbf
Seo-Yurim <yurim2204@naver.com>
2025-01-09 16:20:11 +0900

    design: 너비 조정

--- src/components/Cards/FindTrainerCard.tsx
+++ src/components/Cards/FindTrainerCard.tsx
@@ -1,15 +1,14 @@
 import CardContainer from "../Common/Card/CardContainer";
 import TrainerInfo from "../Common/Card/TrainerInfo/TrainerInfo";
 
-const description = "text-2xl font-semibold";
 /**
  * 임시로 any 타입 지정
  */
 export default function FindTrainerCard({ item }: { item: any }) {
   return (
-    <CardContainer width="95.5rem" gap="1.6rem">
+    <CardContainer width="100%" gap="1.6rem">
       <div className="text-xl font-semibold">칩 넣을 자리</div>
-      <p className={description}>고객님에게 맞춤형 레슨을 해드립니다.</p>
+      <p className="text-2xl font-semibold">고객님에게 맞춤형 레슨을 해드립니다.</p>
       <TrainerInfo
         name={item.name}
         rating={item.rating}
