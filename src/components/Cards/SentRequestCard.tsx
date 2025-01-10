fcbf106263f52c5b052009321391ac229fd167f9
mingmungXD <rex02033@gmail.com>
2025-01-10 11:27:30 +0900

    수정사항 변경

--- src/components/Cards/SentRequestCard.tsx
+++ src/components/Cards/SentRequestCard.tsx
@@ -11,7 +11,7 @@ import { HorizontalLine } from "../Common/Line";
 
 export default function SentRequestCard({ item }: { item: any }) {
   return (
-    <CardContainer width="95.5rem" gap="1.6rem">
+    <CardContainer width="100%" gap="1.6rem">
       <div className="flex justify-between">
         <p className="text-xl font-semibold">칩 넣을 자리</p>
         <p className="text-gray-500 text-xs font-normal">1시간 전</p>
