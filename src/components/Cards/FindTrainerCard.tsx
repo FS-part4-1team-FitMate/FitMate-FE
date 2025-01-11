35b6090d0aceaa08dd55e6694b07085baf76ab71
Seo-Yurim <yurim2204@naver.com>
2025-01-12 03:22:07 +0900

    design: 강사님 찾기 카드에 칩 추가

--- src/components/Cards/FindTrainerCard.tsx
+++ src/components/Cards/FindTrainerCard.tsx
@@ -1,3 +1,5 @@
+import { LessonType } from "@/types/types";
+import ChipLessonType from "../Chip/ChipLessonType";
 import CardContainer from "../Common/Card/CardContainer";
 import TrainerInfo from "../Common/Card/TrainerInfo/TrainerInfo";
 
@@ -7,10 +9,10 @@ import TrainerInfo from "../Common/Card/TrainerInfo/TrainerInfo";
 export default function FindTrainerCard({ item }: { item: any }) {
   return (
     <CardContainer width="100%" gap="1.6rem">
-      <div className="text-lg font-semibold">칩 넣을 자리</div>
-      <p className="font-semibold pc:text-2xl tablet:text-md mobile:text-md">
-        고객님에게 맞춤형 레슨을 해드립니다.
-      </p>
+      <div className="text-lg font-semibold">
+        <ChipLessonType lessonType={LessonType.REHAB} size="lg" />
+      </div>
+      <p className="text-mdfont-semibold pc:text-2xl">고객님에게 맞춤형 레슨을 해드립니다.</p>
       <TrainerInfo
         name={item.name}
         rating={item.rating}
