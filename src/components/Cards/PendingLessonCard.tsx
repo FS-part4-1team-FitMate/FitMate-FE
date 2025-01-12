4a17cf4bf8842b976d6487ce55221281aab9f2e9
Seo-Yurim <yurim2204@naver.com>
2025-01-12 21:06:34 +0900

    design: 대기중인 페이지 반응형

--- src/components/Cards/PendingLessonCard.tsx
+++ src/components/Cards/PendingLessonCard.tsx
@@ -1,4 +1,6 @@
 import clsx from "clsx";
+import { LessonType } from "@/types/types";
+import ChipLessonType from "../Chip/ChipLessonType";
 import CardContainer from "../Common/Card/CardContainer";
 import LessonInfo from "../Common/Card/LessonInfo";
 import QuotePrice from "../Common/Card/QuotePrice";
@@ -15,7 +17,9 @@ const button = clsx("flex-1", "h-[6.4rem]", "p-[1.6rem] rounded-[1.6rem]", "text
 export default function PendingLessonCard({ item }: { item: any }) {
   return (
     <CardContainer width="100%" gap="2.4rem">
-      <div className="text-2lg font-medium">칩 넣을 자리</div>
+      <div className="text-2lg font-medium">
+        <ChipLessonType lessonType={LessonType.REHAB} size="lg" />
+      </div>
       <TrainerInfo
         rating={item.rating}
         reviewCount={item.reviewCount}
