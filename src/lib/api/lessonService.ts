2c84984313dce8b364a2d5839bfd65e789a1a750
Seo-Yurim <yurim2204@naver.com>
2025-01-13 16:56:59 +0900

    feat: 견적 보내기 api 작성

--- src/lib/api/lessonService.ts
+++ src/lib/api/lessonService.ts
@@ -1,21 +1,5 @@
 import { LessonParams, LessonResult } from "@/types/lesson";
-import instance from "./instance";
-
-async function get(url: string, params = {}) {
-  return instance.get(url, { params });
-}
-
-async function post<T>(url: string, body: T) {
-  return instance.post(url, body);
-}
-
-async function patch<T>(url: string, body: T) {
-  return instance.patch(url, body);
-}
-
-async function remove(url: string) {
-  return instance.delete(url);
-}
+import { get } from "./method";
 
 // 받은 레슨 요청 목록 조회
 export async function getReceiveLesson({
