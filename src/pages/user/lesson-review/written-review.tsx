9cbba97714e59d8e7443eb7cde91a7f0fc89765b
mingmungXD <rex02033@gmail.com>
2025-01-14 17:34:30 +0900

    내가 작성한 리뷰

--- src/pages/user/lesson-review/written-review.tsx
+++ src/pages/user/lesson-review/written-review.tsx
@@ -1,5 +1,7 @@
 import ReviewCard from "@/components/Cards/ReviewCard";
 import Pagination from "@/components/Common/Pagination";
+import Tab from "@/components/Tab";
+import { getReview } from "@/lib/api/ReviewService";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
@@ -9,7 +11,7 @@ export default function ReviewListPage() {
   
     const { data } = useQuery(
       ["reviews", currentPage],
-      () => fetchReviews(currentPage, ITEMS_PER_PAGE),
+      () => getReview({ page: currentPage, limit: ITEMS_PER_PAGE}),
       {
         keepPreviousData: true,
       }
@@ -21,7 +23,7 @@ export default function ReviewListPage() {
   
     return (
       <div className="p-6">
-        <h1 className="text-2xl font-bold mb-6">작성 가능한 리뷰</h1>
+        <Tab />
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
           {data.reviews.map((review: any) => (
             <ReviewCard
