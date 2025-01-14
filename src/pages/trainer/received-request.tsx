805be47ad3a9f59347aa88fdbfe2cef18c8f4373
Seo-Yurim <yurim2204@naver.com>
2025-01-14 11:38:48 +0900

    fix: 코드 정리

--- src/pages/trainer/received-request.tsx
+++ src/pages/trainer/received-request.tsx
@@ -1,12 +1,12 @@
 import { ic_filter_active_sm } from "@/imageExports";
 import clsx from "clsx";
+import { GetServerSideProps } from "next";
 import Image from "next/image";
-import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { getReceiveRequest } from "@/lib/api/lessonService";
 import { GenderFilter, ReceivedRequestFilter, ServiceFilter, UserSort } from "@/types/dropdown";
-import { Lesson, LessonResult } from "@/types/lesson";
+import { Lesson, LessonParams, LessonResult } from "@/types/lesson";
 import RequestLessonCard from "@/components/Cards/RequestLessonCard";
 import CheckboxFilter from "@/components/CheckboxFilter";
 import Loading from "@/components/Common/Loading";
@@ -21,93 +21,17 @@ const serviceFilter: ServiceFilter[] = ["REHAB", "SPORTS", "FITNESS"];
 const genderFilter: GenderFilter[] = ["MALE", "FEMALE"];
 const receivedRequestFilter: ReceivedRequestFilter[] = ["서비스 가능 지역", "지정 견적 요청"];
 
+const container = clsx(
+  "flex flex-col w-full mx-auto py-[2.4rem] px-8",
+  "pc:flex-row pc:gap-[10rem] pc:max-w-[140rem] tablet:max-w-[74.4rem] mobile:max-w-[37.5rem]",
+);
+
 export default function ReceivedRequest() {
-  const router = useRouter();
-  const { query } = router;
-
-  // const [sort, setSort] = useState<string>(query.sort || "");
-  // const [lessonType, setLessonType] = useState<string[]>(query.lesson_type || []);
-  // const [gender, setGender] = useState<string[]>(query.gender || []);
-  // const [region, setRegion] = useState<string[]>(query.region || []);
-  // const [keyword, setKeyword] = useState<string>(query.keyword || "");
   const [keyword, setKeyword] = useState<string>("");
-
   const [isModalopen, setIsModalOpen] = useState<boolean>(false);
+  // 타입 지정 필요
+  const [filteredData, setFilteredData] = useState<Lesson[]>(data.list);
   const [sortOrder, setSortOrder] = useState<string>("레슨 빠른 순");
-  const [filteredData, setFilteredData] = useState<Lesson[]>([]);
-
-  const loaderRef = useRef<HTMLDivElement>(null);
-
-  // const { data, error, isLoading, isError, isFetchingNextPage, hasNextPage, fetchNextPage } =
-  //   useInfiniteQuery<LessonResult>({
-  //     queryKey: ["received-request", query],
-  //     queryFn: ({ pageParam = 1 }) =>
-  //       getReceiveRequest({
-  //         page: pageParam,
-  //         limit: 10,
-  //         order: "start_date",
-  //         sort: sort,
-  //         lesson_type: lessonType.join(","),
-  //         gender: gender.join(","),
-  //         region: region.join(","),
-  //         keyword: keyword || "",
-  //       }),
-  //     getNextPageParam: (lastPage) => {
-  //       return lastPage.hasMore ? lastPage.list.length + 1 : false;
-  //     },
-  //   });
-
-  // 무한 스크롤
-  // useEffect(() => {
-  //   const observer = new IntersectionObserver(
-  //     (entries) => {
-  //       if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
-  //         fetchNextPage();
-  //       }
-  //     },
-  //     {
-  //       rootMargin: "100px",
-  //     },
-  //   );
-
-  //   if (loaderRef.current) {
-  //     observer.observe(loaderRef.current);
-  //   }
-
-  //   return () => {
-  //     if (loaderRef.current) {
-  //       observer.unobserve(loaderRef.current);
-  //     }
-  //   };
-  // }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
-
-  // // 쿼리 파라미터 업데이트
-  // const updateQueryParams = () => {
-  //   router.push(
-  //     {
-  //       pathname: router.pathname,
-  //       query: {
-  //         ...query,
-  //         keyword,
-  //         sort: sortOrder,
-  //         lesson_type: lessonType.join(","),
-  //         gender: gender.join(","),
-  //         region: region.join(","),
-  //       },
-  //     },
-  //     undefined,
-  //     { shallow: true },
-  //   );
-  // };
-
-  // const handleSortChange = (sort: string) => {
-  //   setSortOrder(sort);
-  //   updateQueryParams();
-  // };
-
-  // const handleSearch = () => {
-  //   updateQueryParams();
-  // };
 
   // 정렬 처리 함수
   const handleSortChange = (sort: string) => {
@@ -135,38 +59,14 @@ export default function ReceivedRequest() {
     setFilteredData(filtered);
   };
 
-  // const handleCheckboxChange = (type: string, selectedOptions: string[]) => {
-  //   if (type === "lessonType") {
-  //     setLessonType(selectedOptions);
-  //   } else if (type === "gender") {
-  //     setGender(selectedOptions);
-  //   } else if (type === "region") {
-  //     setRegion(selectedOptions);
-  //   }
-  //   updateQueryParams();
-  // };
-
-  // if (isLoading) {
-  //   return <Loading />;
-  // }
-
-  // if (isError) {
-  //   return <div>데이터를 불러오는 중 오류가 발생하였습니다.</div>;
-  // }
-
   return (
     <div className="flex flex-col gap-[2.4rem] max-w-[192rem] m-auto">
       <Title title="받은 요청" />
-      <div
-        className={clsx(
-          "flex flex-col w-full mx-auto py-[2.4rem] px-8",
-          "pc:flex-row pc:gap-[10rem] pc:max-w-[140rem] tablet:max-w-[74.4rem] mobile:max-w-[37.5rem]",
-        )}
-      >
+      <div className={container}>
         <div className="flex flex-col gap-[4.6rem]">
           <div className="hidden flex-col gap-[5rem] pc:flex">
             <CheckboxFilter
-              items={filteredData}
+              items={data.list}
               label="운동 유형"
               options={serviceFilter}
               filterType="lessonType"
@@ -189,7 +89,7 @@ export default function ReceivedRequest() {
               </div>
             </div>
           </div>
-          {data.list.map((item) => (
+          {filteredData.map((item) => (
             <div className="flex flex-col gap-[4.8rem]" key={item.id}>
               <RequestLessonCard item={item} />
             </div>
