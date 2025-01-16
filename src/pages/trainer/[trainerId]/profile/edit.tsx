3055891419dfc9f472f5f6daa1cb6b0d24f77e57
kipid <kipacti@gmail.com>
2025-01-16 19:29:29 +0900

    Update edit.tsx

--- src/pages/trainer/[trainerId]/profile/edit.tsx
+++ src/pages/trainer/[trainerId]/profile/edit.tsx
@@ -1,32 +1,33 @@
 import { useSetUser, useUser } from "@/contexts/UserProvider";
-import { ic_designate_md, ic_edit_sm, ic_visibility_off, ic_visibility_on } from "@/imageExports";
+import { ic_designate_md, ic_edit_sm } from "@/imageExports";
 import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { patchProfile } from "@/lib/api/authService";
-import instance from "@/lib/api/instance";
-import { PHONE_REGEX, PWD_REGEX, error_class, note_class, profile_menu } from "@/types/constants";
-import { Gender, LessonType, LocationType, Profile, ProfileEdittable, Region } from "@/types/types";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { getProfile, patchProfile } from "@/lib/api/authService";
+import { PHONE_REGEX, error_class, note_class, profile_menu } from "@/types/constants";
+import { Gender, LessonType, ProfileEdittable, Region } from "@/types/types";
 import Button from "@/components/Common/Button";
 import Input from "@/components/Common/Input";
-import InputPassword from "@/components/Common/InputPassword";
 import PopUp from "@/components/Common/PopUp";
 import Textarea from "@/components/Common/Textarea";
 import Regions from "@/components/Profile/Regions";
 import ImageUploader from "@/components/SignUp/ImageUploader";
 
-type FormType = Partial<ProfileEdittable> & {
-  currPassword: string;
-  password: string;
-  passwordConfirm: string;
-};
+type FormType = Partial<ProfileEdittable>;
+//  & {
+//   currPassword: string;
+//   password: string;
+//   passwordConfirm: string;
+// };
 
 function ProfileEdit() {
-  const [curPwdIsVisible, setCurPwdIsVisible] = useState(false);
-  const [pwdIsVisible, setPwdIsVisible] = useState(false);
-  const [pwdCfmIsVisible, setPwdCfmIsVisible] = useState(false);
+  const queryClient = useQueryClient();
+  // const [curPwdIsVisible, setCurPwdIsVisible] = useState(false);
+  // const [pwdIsVisible, setPwdIsVisible] = useState(false);
+  // const [pwdCfmIsVisible, setPwdCfmIsVisible] = useState(false);
   const router = useRouter();
   const { trainerId } = router.query;
   const [selectedRegion, setSelectedRegion] = useState<Region[]>([]);
@@ -37,7 +38,7 @@ function ProfileEdit() {
   >(null);
   const {
     register,
-    watch,
+    // watch,
     handleSubmit,
     reset,
     formState: { errors },
@@ -50,7 +51,6 @@ function ProfileEdit() {
       name: user?.profile?.name,
       phone: user?.profile?.phone,
       gender: user?.profile?.gender,
-      lessonType: user?.profile?.lessonType,
       certification: user?.profile?.certification,
       certificationCount: 0,
       region: user?.profile?.region,
@@ -58,15 +58,33 @@ function ProfileEdit() {
       experience: user?.profile?.experience,
       intro: user?.profile?.intro,
       description: user?.profile?.description,
-      currPassword: "",
-      password: "",
-      passwordConfirm: "",
+      // currPassword: "",
+      // password: "",
+      // passwordConfirm: "",
     } as FormType,
+  });
+  const {
+    data: profileData,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["profile", user?.id],
+    queryFn: () => getProfile(user?.id!),
+    cacheTime: 60 * 60 * 1000,
+    staleTime: 60 * 60 * 1000,
+    enabled: !!user?.id,
   });
 
   useEffect(() => {
-    reset(user?.profile);
-  }, [user]);
+    if (user && profileData) {
+      setUser({
+        ...user,
+        ...profileData,
+      });
+      reset(profileData.profile);
+      setSelectedRegion(profileData.profile.region);
+    }
+  }, [profileData]);
 
   const onSubmit = async (data: FormType) => {
     // data.region = selectedRegion;
@@ -84,10 +102,13 @@ function ProfileEdit() {
       changedData.region = selectedRegion;
     }
     console.log(changedData); // TODO: remove this.
+    if (changedData.experience) {
+      changedData.experience = Number(changedData.experience);
+    }
     let profileImageFileToUpload;
-    if ("profileImage" in changedData) {
+    if ("profileImage" in changedData && changedData?.profileImage?.length) {
       const profileImage = changedData.profileImage;
-      if (profileImage instanceof FileList) {
+      if (profileImage instanceof FileList && profileImage[0]?.name) {
         profileImageFileToUpload = profileImage[0];
         changedData.profileImageCount = 1;
         changedData.contentType = profileImage[0].type;
@@ -95,9 +116,9 @@ function ProfileEdit() {
       }
     }
     let certificationFileToUpload;
-    if ("certification" in changedData) {
+    if ("certification" in changedData && changedData?.certification?.length) {
       const certification = changedData.certification;
-      if (certification instanceof FileList) {
+      if (certification instanceof FileList && certification[0]?.name) {
         certificationFileToUpload = certification[0];
         changedData.certificationCount = 1;
         changedData.contentType = certification[0].type;
@@ -105,28 +126,32 @@ function ProfileEdit() {
       }
     }
     try {
+      delete changedData.updatedAt;
+      console.log("changedData: ", changedData); // TODO: remove this.
       const userData = await patchProfile(user?.id!, changedData);
+      console.log(userData); // TODO: remove this.
       if ("profileImagePresignedUrl" in userData) {
         const result = await axios.put(
           userData.profileImagePresignedUrl as string,
           profileImageFileToUpload,
         );
-        console.log(result);
+        console.log(result); // TODO: remove this.
       }
       if ("certificationPresignedUrl" in userData) {
         const result = await axios.put(
           userData.certificationPresignedUrl as string,
           certificationFileToUpload,
         );
-        console.log(result);
+        console.log(result); // TODO: remove this.
       }
-      if ("profile" in userData) {
-        const profile = userData.profile! as Profile;
-        const userDataLS = JSON.parse(localStorage.getItem("userData")!);
-        userDataLS.user = { ...user, profile };
-        setUser(userDataLS.user);
-        localStorage.setItem("userData", JSON.stringify(userDataLS));
-      }
+      const userDataLS = JSON.parse(localStorage.getItem("userData")!);
+      userDataLS.user = { ...user, ...userData };
+      setUser((prev) => userDataLS.user);
+      localStorage.setItem("userData", JSON.stringify(userDataLS));
+      queryClient.invalidateQueries({
+        queryKey: ["profile", user?.id],
+      });
+      router.push(`/trainer/${user?.id}/profile`);
     } catch (err) {
       setError({ message: (err as Error).message });
     }
@@ -145,7 +170,9 @@ function ProfileEdit() {
             id="profileImage"
             label="프로필 이미지"
             defImage={
-              user?.profile?.profileImage ? (user?.profile?.profileImage as string) : undefined
+              profileData?.profileImagePresignedUrl
+                ? profileData.profileImagePresignedUrl
+                : undefined
             }
             register={register("profileImage")}
           />
@@ -241,7 +268,9 @@ function ProfileEdit() {
             width={300}
             height={300}
             defImage={
-              user?.profile?.certification ? user?.profile?.certification : ic_designate_md.src
+              profileData?.certificationPresignedUrl
+                ? profileData.certificationPresignedUrl
+                : ic_designate_md.src
             }
           />
           <hr className="w-full border-[1px] border-solid border-gray-300" />
@@ -263,43 +292,9 @@ function ProfileEdit() {
           </div>
           {errors.region && <p className={error_class}>{errors.region.message}</p>}
           <hr className="w-full border-[1px] border-solid border-gray-300" />
-          <div className={profile_menu}>
-            <div className="flex flex-col gap-[8px]">
-              <label className="text-lg font-semibold">강의 주소 타입</label>
-              <p className={note_class}>* 반드시 하나 이상을 선택해 주세요!</p>
-            </div>
-            <div className="flex gap-[16px]">
-              <label className="text-lg">
-                <input
-                  {...register("locationType", {
-                    validate: (value) =>
-                      (value && value.length > 0) || "반드시 하나 이상을 선택해야 합니다.",
-                  })}
-                  type="checkbox"
-                  name="locationType"
-                  value={LocationType.OFFLINE}
-                />
-                &nbsp;오프라인
-              </label>
-              <label className="text-lg">
-                <input
-                  {...register("locationType", {
-                    validate: (value) =>
-                      (value && value.length > 0) || "반드시 하나 이상을 선택해야 합니다.",
-                  })}
-                  type="checkbox"
-                  name="locationType"
-                  value={LocationType.ONLINE}
-                />
-                &nbsp;온라인
-              </label>
-            </div>
-            {errors.locationType && <p className={error_class}>{errors.locationType.message}</p>}
-          </div>
-          <hr className="w-full border-[1px] border-solid border-gray-300" />
           <Input
             id="experience"
-            label="경력 (연, 소수점 입력 가능)"
+            label="경력 (연, 소수점 입력 불가)"
             type="number"
             register={register("experience", {
               min: {
@@ -332,7 +327,7 @@ function ProfileEdit() {
             placeholder="상세 설명을 입력해 주세요."
           ></Textarea>
           {errors.description && <p className={error_class}>{errors.description.message}</p>}
-          <hr className="w-full border-[1px] border-solid border-gray-300" />
+          {/* <hr className="w-full border-[1px] border-solid border-gray-300" />
           <InputPassword
             id="currPassword"
             label="현재 비밀번호"
@@ -382,7 +377,7 @@ function ProfileEdit() {
           />
           {errors.passwordConfirm && (
             <p className="text-red-400 text-sm">{errors.passwordConfirm.message}</p>
-          )}
+          )} */}
           <hr className="w-full border-[1px] border-solid border-gray-300" />
           <Button type="submit" className="w-full bg-blue-500 text-white disabled:bg-slate-600">
             수정하기 <Image src={ic_edit_sm} width={24} height={24} alt="Edit" />
@@ -390,7 +385,9 @@ function ProfileEdit() {
           <Button
             type="button"
             className="w-full border border-solid border-slate-800 bg-slate-200 text-black-500"
-            onClick={() => router.push(`/trainer/${trainerId}/profile`)}
+            onClick={() => {
+              router.push(`/trainer/${trainerId}/profile`);
+            }}
           >
             취소하기
           </Button>
