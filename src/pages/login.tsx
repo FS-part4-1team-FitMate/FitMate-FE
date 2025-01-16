c758e04ef354e0f2e8ea7ca43db2750666c7c062
kipid <kipacti@gmail.com>
2025-01-16 16:46:35 +0900

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -13,7 +13,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { postLogin } from "@/lib/api/authService";
+import { getProfile, postLogin } from "@/lib/api/authService";
 import { EMAIL_REGEX } from "@/types/constants";
 import { Role } from "@/types/types";
 import PopUp from "@/components/Common/PopUp";
@@ -46,14 +46,15 @@ function LogIn() {
 
   useEffect(() => {
     if (user?.id) {
+      const userData = JSON.parse(localStorage.getItem("userData")!);
       if (user.role === Role.USER) {
-        if ("profile" in user && user.profile?.name) {
+        if (user.hasProfile) {
           router.push("/user/my-lesson/active-lesson");
         } else {
           router.push("/user/profile/regist");
         }
       } else if (user.role === Role.TRAINER) {
-        if ("profile" in user && user.profile?.name) {
+        if (user.hasProfile) {
           router.push("/trainer/managing-request/sent-request");
         } else {
           router.push(`/trainer/${user.id}/profile/regist`);
@@ -68,15 +69,16 @@ function LogIn() {
       console.log(userData); // TODO: remove this.
       if ("user" in userData) {
         const { user } = userData;
+        user.hasProfile = userData.hasProfile;
         setUser(user);
         if (user.role === Role.USER) {
-          if ("profile" in user && user.profile?.name) {
+          if (user.hasProfile) {
             router.push("/user/my-lesson/active-lesson");
           } else {
             router.push("/user/profile/regist");
           }
         } else if (user.role === Role.TRAINER) {
-          if ("profile" in user && user.profile?.name) {
+          if (user.hasProfile) {
             router.push("/trainer/managing-request/sent-request");
           } else {
             router.push(`/trainer/${user.id}/profile/regist`);
