ac878ccefa2d323efe5be9758c8a2bf649d7596c
Seo-Yurim <yurim2204@naver.com>
2025-01-17 11:18:49 +0900

    feat: 무한 스크롤 기능 구현

--- src/pages/trainer/received-request.tsx
+++ src/pages/trainer/received-request.tsx
@@ -1,8 +1,7 @@
 import { ic_filter_active_sm } from "@/imageExports";
 import clsx from "clsx";
 import Image from "next/image";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getReceiveRequest } from "@/lib/api/lessonService";
 import { GenderFilter, ServiceFilter, UserSort } from "@/types/dropdown";
@@ -14,6 +13,7 @@ import Search from "@/components/Common/Search";
 import Title from "@/components/Common/Title";
 import Dropdown from "@/components/Dropdown/Dropdown";
 import MobileFilter from "@/components/Modal/MobileFilter";
+import InfiniteScroll from "react-infinite-scroller";
 
 const userSort: UserSort[] = ["레슨 빠른 순", "레슨 느린 순", "최근 요청 순"];
 const serviceFilter: ServiceFilter[] = ["REHAB", "SPORTS", "FITNESS"];
@@ -21,96 +21,37 @@ const genderFilter: GenderFilter[] = ["MALE", "FEMALE"];
 const receivedRequestFilter: string[] = ["REGION", "DIRECT"];
 
 export default function ReceivedRequest() {
-  const router = useRouter();
-  const query = router.query;
-
-  const [order, setOrder] = useState<string>(query.order?.toString() || "start_date");
-  const [sort, setSort] = useState<string>(query.sort?.toString() || "asc");
-  const [keyword, setKeyword] = useState<string>(query.keyword?.toString() || "");
-  const [lessonType, setLessonType] = useState<string>(query.lesson_type?.toString() || "");
-  const [gender, setGender] = useState<string>(query.gender?.toString() || "");
-  const [region, setRegion] = useState<string>(query.region?.toString() || "");
-  const [isDirectQuote, setIsDirectQuote] = useState<boolean>(false);
-
-  const filters = { order, sort, lessonType, gender, region, keyword };
-
-  const { data, isLoading, isError, isFetchingNextPage, hasNextPage, fetchNextPage } =
-    useInfiniteQuery<LessonResult>({
-      queryKey: ["received-request", filters],
-      queryFn: ({ pageParam = 1 }) =>
+  const { data, isLoading, isError, hasNextPage, fetchNextPage } =
+    useInfiniteQuery<LessonResult>(
+     ["received-request"],
+      ({ pageParam = 1 }) =>
         getReceiveRequest({
           page: pageParam,
           limit: 10,
-          order: filters.order,
-          sort: filters.sort,
-          lesson_type: filters.lessonType || undefined,
-          gender: filters.gender || undefined,
-          region: filters.region || undefined,
-          keyword: filters.keyword,
         }),
-      getNextPageParam: (lastPage) => {
-        return lastPage.hasMore ? lastPage.list.length + 1 : false;
+      {
+        getNextPageParam: (lastPage, allPages) => {
+        return lastPage.hasMore ? allPages.length + 1 : undefined;
       },
-    });
-
+    }
+    );
+    
   const receivedList = data?.pages.flatMap((page) => page.list) ?? [];
-  const totalCount = data?.pages.flatMap((page) => page.totalCount) ?? [];
-  const [filteredData, setFilteredData] = useState<Lesson[]>(receivedList);
+  const totalCount =  data?.pages[0]?.totalCount ?? 0;
   const [isModalopen, setIsModalOpen] = useState<boolean>(false);
-
-  useEffect(() => {
-    router.push(
-      {
-        pathname: router.pathname,
-        query: {},
-      },
-      undefined,
-      { shallow: true },
-    );
-  }, []);
-
-  // 빈 값 필터링 후 쿼리 파라미터 업데이트
-  const updateQueryParams = (params: { [key: string]: string | null }) => {
-    const filteredParams = Object.fromEntries(
-      Object.entries(params).filter(([key, value]) => value !== null && value !== ""),
-    );
-
-    const formattedParams = Object.fromEntries(
-      Object.entries(filteredParams).map(([key, value]) => [
-        key === "lessonType" ? "lesson_type" : key,
-        value,
-      ]),
-    );
-    router.push(
-      {
-        pathname: router.pathname,
-        query: { ...query, ...formattedParams },
-      },
-      undefined,
-      { shallow: true },
-    );
-  };
 
   // 정렬 처리 함수
   const handleSortChange = (order: string, sort: string) => {
-    setOrder(order);
-    setSort(sort);
-    updateQueryParams({ order, sort });
+
   };
 
   // 검색 처리 함수
   const handleSearch = (searchTerm: string) => {
-    setKeyword(searchTerm);
-    updateQueryParams({ keyword: searchTerm });
+  
   };
 
   // 필터 처리 함수
   const handleFilterChange = (filtered: Lesson[]) => {
-    setFilteredData(filtered);
-  };
-
-  if (isLoading) {
-    return <Loading />;
   }
 
   if (isError) {
@@ -164,11 +105,14 @@ export default function ReceivedRequest() {
               </div>
             </div>
           </div>
-          {filteredData.map((item) => (
+          <InfiniteScroll hasMore={hasNextPage} loadMore={() => fetchNextPage()}>
+          {receivedList.map((item) => (
             <div className="flex flex-col gap-[4.8rem]" key={item.id}>
               <RequestLessonCard item={item} />
             </div>
           ))}
+          </InfiniteScroll>
+          {isLoading && <Loading />}
         </div>
       </div>
       {isModalopen && <MobileFilter closeModal={() => setIsModalOpen(false)} />}
