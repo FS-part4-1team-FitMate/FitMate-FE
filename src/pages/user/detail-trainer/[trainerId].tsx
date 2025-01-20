a4c2f0424333a4a75c2a81b2dfd3f4b116eb7939
Seo-Yurim <yurim2204@naver.com>
2025-01-20 17:04:53 +0900

    feat: 데이터 페칭 코드 작성

--- src/pages/user/detail-trainer/[trainerId].tsx
+++ src/pages/user/detail-trainer/[trainerId].tsx
@@ -1,4 +1,6 @@
 import clsx from "clsx";
+import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
 import { LessonType, Region } from "@/types/types";
 import FindTrainerCard from "@/components/Cards/FindTrainerCard";
 import ChipLessonType from "@/components/Chip/ChipLessonType";
@@ -17,10 +19,17 @@ const buttons = "flex flex-row gap-[0.8rem] w-full p-4 pc:flex-col pc:gap-[3.2re
 const button = "h-[5.4rem] p-4 rounded-[1.6rem] font-semibold pc:w-[35.3rem] pc:text-xl";
 
 export default function DetailTrainer() {
+  const router = useRouter();
+  const trainerId = router.query.id;
+
+  const { data, isLoading, isError } = useQuery<Trainer>(["trainer-detail", trainerId], () =>
+    getTrainerInfo(trainerId),
+  );
+
   return (
     <div className={container}>
       <div className={"flex flex-col gap-[2.4rem] w-full pc:gap-16 pc:pr-[10rem]"}>
-        <FindTrainerCard item="" />
+        <FindTrainerCard item={data} />
         <div className="flex flex-col gap-4 pc:hidden">
           <HorizontalLine width="100%" />
           <ShareSNS label="나만 알기엔 아쉬운 강사님인가요?" />
@@ -34,14 +43,14 @@ export default function DetailTrainer() {
         <div className="flex flex-col gap-4 pc:gap-[3.2rem]">
           <h1 className="text-black-400 text-lg font-bold pc:text-2xl">제공 서비스</h1>
           <p className="text-black-400 text-md font-normal pc:text-2lg">
-            <ChipLessonType lessonType={LessonType.SPORTS} size="lg" />
+            <ChipLessonType lessonType={item.lessonType} size="lg" />
           </p>
         </div>
         <HorizontalLine width="100%" />
         <div className="flex flex-col gap-4 pc:gap-[3.2rem]">
           <h1 className="text-black-400 text-lg font-bold pc:text-2xl">서비스 가능 지역</h1>
           <p className="text-black-400 text-md font-normal pc:text-2lg">
-            <ChipRegion region={Region.DAEGU} size="lg" />
+            <ChipRegion region={item.region} size="lg" />
           </p>
         </div>
         <HorizontalLine width="100%" />
@@ -50,7 +59,7 @@ export default function DetailTrainer() {
       <div className="flex flex-col gap-[2.4rem] pc:gap-16">
         <div className="flex flex-col gap-4 pc:gap-[3.2rem]">
           <h1 className="hidden text-xl font-semibold pc:block">
-            김코드 강사님에게 지정 견적을 요청해보세요
+            {item.name} 강사님에게 지정 견적을 요청해보세요
           </h1>
           <div className={buttons}>
             <Button className={`${button} hidden gap-4 border border-line-200 bg-gray-50 pc:flex`}>
