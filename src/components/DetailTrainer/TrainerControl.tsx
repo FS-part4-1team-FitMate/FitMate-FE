e28611e9d2357f0ce4c4253d25ed9189e204800a
Seo-Yurim <yurim2204@naver.com>
2025-01-21 15:49:55 +0900

    feat: 컴포넌트 분리 및 리뷰 차트 UI 구현

---
+++ src/components/DetailTrainer/TrainerControl.tsx
@@ -0,0 +1,24 @@
+import { Profile } from "@/types/types";
+import Button from "../Common/Button";
+import Favorite from "../Common/Card/TrainerInfo/Favorite";
+
+export default function TrainerControl({ profile }: { profile: Profile }) {
+  return (
+    <div className="flex flex-col gap-4 pc:gap-[3.2rem]">
+      <h1 className="hidden text-xl font-semibold pc:block">
+        {profile?.name} 강사님에게 지정 견적을 요청해보세요
+      </h1>
+      <div className="flex flex-row gap-[0.8rem] w-full p-4 pc:flex-col pc:gap-[3.2rem] pc:px-0">
+        <Button className="h-[5.4rem] p-4 rounded-[1.6rem] font-semibold pc:w-[35.3rem] pc:text-xl hidden gap-4 border border-line-200 bg-gray-50 pc:flex">
+          <Favorite /> 강사님 찜하기
+        </Button>
+        <div className="flex justify-center items-center w-[5.4rem] h-[5.4rem] p-4 border border-line-200 rounded-[1.6rem] pc:hidden">
+          <Favorite />
+        </div>
+        <Button className="h-[5.4rem] p-4 rounded-[1.6rem] font-semibold pc:w-[35.3rem] pc:text-xl w-full text-gray-50 bg-blue-300">
+          지정 견적 요청하기
+        </Button>
+      </div>
+    </div>
+  );
+}
--- src/pages/user/detail-trainer/[trainerId].tsx
+++ src/pages/user/detail-trainer/[trainerId].tsx
@@ -1,25 +1,42 @@
 import clsx from "clsx";
-import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
+import { GetServerSideProps } from "next";
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getTrainerInfo } from "@/lib/api/trainerService";
-import { LessonType, Region } from "@/types/types";
 import FindTrainerCard from "@/components/Cards/FindTrainerCard";
-import ChipLessonType from "@/components/Chip/ChipLessonType";
-import ChipRegion from "@/components/Chip/ChipRegion";
-import Button from "@/components/Common/Button";
-import Favorite from "@/components/Common/Card/TrainerInfo/Favorite";
 import { HorizontalLine } from "@/components/Common/Line";
 import Loading from "@/components/Common/Loading";
+import Pagination from "@/components/Common/Pagination";
 import ShareSNS from "@/components/Common/ShareSNS";
+import TrainerControl from "@/components/DetailTrainer/TrainerControl";
+import TrainerInfo from "@/components/DetailTrainer/TrainerInfo";
+import TrainerReview from "@/components/DetailTrainer/TrainerReview";
 
-export default function DetailTrainer() {
-  const router = useRouter();
-  const { trainerId } = router.query;
+interface Params extends ParsedUrlQuery {
+  trainerId: string;
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { trainerId } = context.params as Params;
 
   if (!trainerId) {
-    return <div>Loading...</div>;
+    return {
+      props: {
+        trainerId: null,
+      },
+    };
   }
 
+  return {
+    props: {
+      trainerId,
+    },
+  };
+};
+
+export default function DetailTrainer({ trainerId }: { trainerId: string | null }) {
+  const [currentPage, setCurrentPage] = useState<number>(1);
   const { data, isLoading, isError } = useQuery(
     ["trainer-detail", trainerId],
     () => getTrainerInfo(trainerId as string),
@@ -35,7 +52,7 @@ export default function DetailTrainer() {
   return (
     <div
       className={clsx(
-        "relative flex flex-col justify-between gap-16 m-auto mt-[2.4rem] px-8",
+        "relative flex flex-col justify-between gap-16 m-auto mt-[2.4rem] mb-16 px-8",
         "pc:flex-row pc:gap-0 pc:mt-[5.6rem] pc:max-w-[144rem]",
         "tablet:max-w-[74.4rem] mobile:max-w-[37.5rem]",
       )}
@@ -47,50 +64,13 @@ export default function DetailTrainer() {
           <ShareSNS label="나만 알기엔 아쉬운 강사님인가요?" />
         </div>
         <HorizontalLine width="100%" />
-        <div className="flex flex-col gap-4 pc:gap-[3.2rem]">
-          <h1 className="text-black-400 text-lg font-bold pc:text-2xl">상세설명</h1>
-          <p className="text-black-400 text-md font-normal pc:text-2lg">
-            {trainerInfo?.description}
-          </p>
-        </div>
-        <HorizontalLine width="100%" />
-        <div className="flex flex-col gap-4 pc:gap-[3.2rem]">
-          <h1 className="text-black-400 text-lg font-bold pc:text-2xl">제공 서비스</h1>
-          <div className="text-black-400 text-md font-normal pc:text-2lg">
-            {trainerInfo?.lessonType?.map((lessonType: LessonType, index: number) => (
-              <ChipLessonType key={index} lessonType={lessonType} size="lg" />
-            ))}
-          </div>
-        </div>
-        <HorizontalLine width="100%" />
-        <div className="flex flex-col gap-4 pc:gap-[3.2rem]">
-          <h1 className="text-black-400 text-lg font-bold pc:text-2xl">서비스 가능 지역</h1>
-          <div className="text-black-400 text-md font-normal pc:text-2lg">
-            {trainerInfo?.region?.map((region: Region, index: number) => (
-              <ChipRegion key={index} region={region} size="lg" />
-            ))}
-          </div>
-        </div>
-        <HorizontalLine width="100%" />
-        <div className="text-black-400 text-lg font-bold pc:text-2xl">평점 및 리뷰 들어갈 부분</div>
+        <TrainerInfo profile={trainerInfo} />
+        {/* 리뷰 리스트 api 연결해야함 */}
+        <TrainerReview reviewList={trainerInfo} />
+        <Pagination currentPage={currentPage} totalPages={5} onPageChange={setCurrentPage} />
       </div>
       <div className="flex flex-col gap-[2.4rem] pc:gap-16">
-        <div className="flex flex-col gap-4 pc:gap-[3.2rem]">
-          <h1 className="hidden text-xl font-semibold pc:block">
-            {trainerInfo?.name} 강사님에게 지정 견적을 요청해보세요
-          </h1>
-          <div className="flex flex-row gap-[0.8rem] w-full p-4 pc:flex-col pc:gap-[3.2rem] pc:px-0">
-            <Button className="h-[5.4rem] p-4 rounded-[1.6rem] font-semibold pc:w-[35.3rem] pc:text-xl hidden gap-4 border border-line-200 bg-gray-50 pc:flex">
-              <Favorite /> 강사님 찜하기
-            </Button>
-            <div className="flex justify-center items-center w-[5.4rem] h-[5.4rem] p-4 border border-line-200 rounded-[1.6rem] pc:hidden">
-              <Favorite />
-            </div>
-            <Button className="h-[5.4rem] p-4 rounded-[1.6rem] font-semibold pc:w-[35.3rem] pc:text-xl w-full text-gray-50 bg-blue-300">
-              지정 견적 요청하기
-            </Button>
-          </div>
-        </div>
+        <TrainerControl profile={trainerInfo} />
         <div className="hidden flex-col gap-16 pc:flex">
           <HorizontalLine width="100%" />
           <ShareSNS label="나만 알기엔 아쉬운 강사님인가요?" />
