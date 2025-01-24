7e51ad06326864e0b5e7153960ac13d8a264bff8
Seo-Yurim <yurim2204@naver.com>
2025-01-24 10:57:41 +0900

    feat: 진행중인 레슨 데이터 연결

--- src/pages/user/my-lesson/active-lesson/index.tsx
+++ src/pages/user/my-lesson/active-lesson/index.tsx
@@ -1,16 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import { getMyLessonRequest } from "@/lib/api/lessonService";
+import { LessonResult } from "@/types/lesson";
 import ActiveLessonSection from "@/components/ActiveLesson/ActiveLessonSection";
+import Loading from "@/components/Common/Loading";
 
 export default function ActiveLesson() {
-  const items = new Array(1).fill("");
-  const items2 = new Array(0).fill("");
+  const { data, isLoading, isError } = useQuery<LessonResult>(["lesson-list"], () =>
+    getMyLessonRequest(),
+  );
+
+  if (isError) return <div>error!</div>;
+
+  const lessonList = data?.list || [];
+
+  const rehabLessons = lessonList.filter(
+    (lesson) => lesson.lessonType === "REHAB" && lesson.status === "QUOTE_CONFIRMED",
+  );
+  const sportsLessons = lessonList.filter(
+    (lesson) => lesson.lessonType === "SPORTS" && lesson.status === "QUOTE_CONFIRMED",
+  );
+  const fitnessLessons = lessonList.filter(
+    (lesson) => lesson.lessonType === "FITNESS" && lesson.status === "QUOTE_CONFIRMED",
+  );
 
   return (
     <div className="flex max-w-[192rem] m-auto py-16 px-8 bg-bg-100 pc:py-[6.4rem]">
       <div className="flex flex-col gap-8 max-w-[140rem] w-full m-auto">
-        <ActiveLessonSection title="재활운동" items={items} />
-        <ActiveLessonSection title="스포츠" items={items} />
-        <ActiveLessonSection title="피트니스" items={items2} />
+        <ActiveLessonSection title="재활운동" items={rehabLessons} />
+        <ActiveLessonSection title="스포츠" items={sportsLessons} />
+        <ActiveLessonSection title="피트니스" items={fitnessLessons} />
       </div>
+      {isLoading && <Loading />}
     </div>
   );
 }
