ad896e140ec3148131e36f7c486449fd4340d6df
Seo-Yurim <yurim2204@naver.com>
2025-01-24 10:05:15 +0900

    feat: 데이터 변환 및 UI 수정

--- src/components/Cards/PendingLessonCard.tsx
+++ src/components/Cards/PendingLessonCard.tsx
@@ -1,4 +1,4 @@
-import clsx from "clsx";
+import Link from "next/link";
 import { useQuery } from "@tanstack/react-query";
 import { getLessonInfo } from "@/lib/api/lessonService";
 import { getTrainerInfo } from "@/lib/api/trainerService";
@@ -6,7 +6,7 @@ import formatDate from "@/lib/utils/formatDate";
 import formatPrice from "@/lib/utils/formatPrice";
 import { Lesson } from "@/types/lesson";
 import { Quote } from "@/types/quote";
-import { Trainer } from "@/types/trainer";
+import { Profile } from "@/types/trainer";
 import { LessonType, LocationType, locationType_trans } from "@/types/types";
 import ChipLessonType from "../Chip/ChipLessonType";
 import CardContainer from "../Common/Card/CardContainer";
@@ -15,15 +15,14 @@ import QuotePrice from "../Common/Card/QuotePrice";
 import TrainerInfo from "../Common/Card/TrainerInfo/TrainerInfo";
 import Loading from "../Common/Loading";
 
-const buttons = "flex gap-[1.1rem] pc:flex-row tablet:flex-row mobile:flex-col";
-const button = clsx("flex-1", "h-[6.4rem]", "p-[1.6rem] rounded-[1.6rem]", "text-xl font-semibold");
-
 export default function PendingLessonCard({ item }: { item: Quote }) {
   const {
     data: trainer,
     isLoading: isTrainerLoading,
     isError: isTrainerError,
-  } = useQuery<Trainer>(["trainer-info"], () => getTrainerInfo(item.trainerId));
+  } = useQuery<Profile>(["trainer-info", item.trainerId], () => getTrainerInfo(item.trainerId), {
+    enabled: !!item.trainerId,
+  });
 
   const {
     data: lesson,
@@ -34,33 +33,50 @@ export default function PendingLessonCard({ item }: { item: Quote }) {
   if (isTrainerLoading || isLessonLoading) {
     return <Loading />;
   }
+
+  console.log(trainer);
   if (isTrainerError) return <div>트레이너 정보를 불러오지 못했습니다.</div>;
   if (isLessonError) return <div>레슨 정보를 불러오지 못했습니다.</div>;
+
+  const trainerInfo = trainer?.profile ?? [];
+
   return (
     <CardContainer width="100%" gap="2.4rem">
       <div className="text-2lg font-medium">
         <ChipLessonType lessonType={LessonType.REHAB} size="lg" />
       </div>
       <TrainerInfo
-        name={trainer?.nickname}
-        rating={trainer?.profile.rating}
-        reviewCount={trainer?.profile.reviewCount}
-        experience={trainer?.profile.experience}
-        lessonCount={trainer?.profile.lessonCount}
-        isFavorited={trainer?.isFavorite}
-        favoriteCount={trainer?._count?.favoritedByUsers}
+        name={trainerInfo?.name}
+        rating={trainerInfo?.rating || 0}
+        reviewCount={trainerInfo?.reviewCount || 0}
+        experience={trainerInfo?.experience || 0}
+        lessonCount={trainerInfo?.lessonCount || 0}
+        isFavorited={true}
+        favoriteCount={23}
       />
-      <LessonInfo
-        startDate={formatDate(lesson?.startDate)}
-        endDate={formatDate(lesson?.endDate)}
-        locationType={locationType_trans[lesson?.locationType as LocationType]}
-        address={lesson?.roadAddress}
-      />
+      <Link href={`/user/my-lesson/pending-request/${item.id}`}>
+        <LessonInfo
+          startDate={formatDate(lesson?.startDate)}
+          endDate={formatDate(lesson?.endDate)}
+          locationType={locationType_trans[lesson?.locationType as LocationType]}
+          address={lesson?.roadAddress}
+        />
+      </Link>
       <QuotePrice price={formatPrice(item.price)} />
-      <div className={buttons}>
-        <button className={`${button} text-gray-50 bg-blue-300`}>견적 확정하기</button>
-        <button className={`${button} border border-blue-300 text-blue-300 bg-gray-50`}>
-          상세보기
+      <div className="flex gap-[1.1rem] pc:flex-row tablet:flex-row mobile:flex-col">
+        <button
+          className={
+            "flex-1 h-[6.4rem] p-[1.6rem] rounded-[1.6rem] text-xl font-semibold text-gray-50 bg-blue-300"
+          }
+        >
+          견적 확정하기
+        </button>
+        <button
+          className={
+            "flex-1 h-[6.4rem] p-[1.6rem] rounded-[1.6rem] text-xl font-semibold border border-blue-300 text-blue-300 bg-gray-50"
+          }
+        >
+          반려하기
         </button>
       </div>
     </CardContainer>
--- src/components/Common/Card/LessonInfo.tsx
+++ src/components/Common/Card/LessonInfo.tsx
@@ -2,10 +2,8 @@ import { clsx } from "clsx";
 import { VerticalLine } from "../Line";
 
 const lesson_detail = clsx(
-  "flex",
-  "pc:gap-[1.6rem] tablet:gap-[1.4rem] mobile:gap-[1.4rem]",
-  "pc:items-center tablet:items-start mobile:items-start",
-  "pc:flex-row tablet:flex-col mobile:flex-col",
+  "flex flex-col justify-start items-start gap-[1.4rem]",
+  "pc:gap-[1.6rem]",
 );
 const info_text = "flex items-center gap-[0.8rem] text-md font-medium pc:gap-[1.6rem] pc:text-2lg";
 
@@ -27,29 +25,28 @@ export default function LessonInfo({ startDate, endDate, locationType, address }
 
   return (
     <div className={lesson_detail}>
-      <div className={info_text}>
-        <div className="w-fit py-[0.2rem] px-[0.6rem] rounded-[0.4rem] bg-bg-400 pc:py-[0.4rem]">
-          <p className="text-gray-500 text-md font-medium pc:text-2lg">레슨 시작일</p>
-        </div>
-        <p>{startDate}</p>
-      </div>
-      <div className="pc:block tablet:hidden mobile:hidden">
-        <VerticalLine height="1.5rem" />
-      </div>
-      <div className="flex items-center pc:gap-[1.6rem] tablet:gap-[1.4rem] mobile:gap-[1.4rem]">
+      <div className="flex items-center gap-[1.4rem] pc:flex-row tablet:flex-row mobile:flex-col">
         <div className={info_text}>
           <div className="w-fit py-[0.2rem] px-[0.6rem] rounded-[0.4rem] bg-bg-400 pc:py-[0.4rem]">
-            <p className="text-gray-500 text-md font-medium pc:text-2lg">레슨 종료일</p>
+            <p className="text-nowrap text-gray-500 text-md font-medium pc:text-2lg">레슨 시작일</p>
+          </div>
+          <p>{startDate}</p>
+        </div>
+        <div className="pc:block tablet:hidden mobile:hidden">
+          <VerticalLine height="1.5rem" />
+        </div>
+        <div className={info_text}>
+          <div className="w-fit py-[0.2rem] px-[0.6rem] rounded-[0.4rem] bg-bg-400 pc:py-[0.4rem]">
+            <p className="text-nowrap text-gray-500 text-md font-medium pc:text-2lg">레슨 종료일</p>
           </div>
           <p>{endDate}</p>
         </div>
-        <VerticalLine height="1.5rem" />
-        <div className={info_text}>
-          <div className="w-fit py-[0.2rem] px-[0.6rem] rounded-[0.4rem] bg-bg-400 pc:py-[0.4rem]">
-            <p className="text-gray-500 text-md font-medium pc:text-2lg">레슨 장소</p>
-          </div>
-          <p>{getLocation()}</p>
+      </div>
+      <div className={info_text}>
+        <div className="w-fit py-[0.2rem] px-[0.6rem] rounded-[0.4rem] bg-bg-400 pc:py-[0.4rem]">
+          <p className="text-nowrap text-gray-500 text-md font-medium pc:text-2lg">레슨 장소</p>
         </div>
+        <p>{getLocation()}</p>
       </div>
     </div>
   );
