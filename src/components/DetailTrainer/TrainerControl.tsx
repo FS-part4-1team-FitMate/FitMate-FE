b5a8ad1f78c8d00f4d42d22e66ecbb337efe2494
kipid <kipacti@gmail.com>
2025-02-04 12:12:09 +0900

    components\DetailTrainer\TrainerControl.tsx

--- src/components/DetailTrainer/TrainerControl.tsx
+++ src/components/DetailTrainer/TrainerControl.tsx
@@ -9,8 +9,9 @@ import Favorite from "../Common/Favorite";
 export default function TrainerControl({ profile }: { profile: Profile }) {
   const queryClient = useQueryClient();
   const router = useRouter();
-  const { data: myLessonList } = useQuery<MyLessonResult>(["my-lesson"], () =>
-    getMyLessonRequest(),
+  const { data: myLessonList } = useQuery<MyLessonResult>(
+    ["my-lesson", { page: 1, limit: 1 }],
+    () => getMyLessonRequest({ page: 1, limit: 1 }),
   );
 
   const lessonId = myLessonList?.list[0]?.id;
