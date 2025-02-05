0bbee62254ca2fca13c2445fb1d5284bda5883d8
mingmungXD <rex02033@gmail.com>
2025-02-06 08:53:12 +0900

    내가 쓴 리뷰 카드

--- src/components/Cards/MyReviewCard.tsx
+++ src/components/Cards/MyReviewCard.tsx
@@ -1,4 +1,3 @@
-import { VerticalLine } from "../Common/Line";
 import StarPoints from "../Common/StarPoints";
 
 interface Props {
@@ -6,20 +5,25 @@ interface Props {
   createdAt: string;
   nickname: string;
   content: string;
+  nickName: string;
+  price: number;
 }
 
-function ReviewCard({ rating, createdAt, content }: Props) {
+function MyReviewCard({ rating, createdAt, content, nickName, price }: Props) {
   return (
-    <div className="flex flex-col gap-[12px]">
-      <div className="flex justify-start items-center">
-        <div className="text-md font-medium">{}</div>
-        <VerticalLine height="14px" />
-        <div className="text-md text-slate-500">{createdAt}</div>
+    <div className="flex flex-col gap-[12px] border border-gray-200 p-[16px] rounded-lg shadow-sm bg-white">
+      <div className="flex flex-col gap-[4px]">
+        <div className="text-md text-slate-500">작성일: {createdAt}</div>
+        <div className="text-md font-bold">{nickName}</div>
+        <div className="text-md text-gray-600">견적가: {price.toLocaleString()}원</div>
+      </div>
+      <div className="flex justify-between items-center mt-[8px]">
+        
       </div>
       <StarPoints rating={rating / 5.0} />
-      <pre className="text-md text-wrap font-pretendard">{content}</pre>
+      <pre className="text-md text-wrap font-pretendard text-gray-800 mt-[8px]">{content}</pre>
     </div>
   );
 }
 
-export default ReviewCard;
+export default MyReviewCard;
