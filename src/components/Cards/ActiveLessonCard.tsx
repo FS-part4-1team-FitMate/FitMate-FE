b086eeb0c3eae33ff3b1424b35e281df8f8be446
Seo-Yurim <yurim2204@naver.com>
2025-02-06 16:22:19 +0900

    refactor: 트레이너, 좋아요 쿼리 호출 방식 변경

--- src/components/Cards/ActiveLessonCard.tsx
+++ src/components/Cards/ActiveLessonCard.tsx
@@ -1,50 +1,31 @@
 import toast from "react-hot-toast";
-import { useQuery } from "@tanstack/react-query";
-import { getTrainerInfo } from "@/lib/api/trainerService";
-import { getFavorite } from "@/lib/api/userService";
+import { useGetFavoriteInfo, useGetTrainer } from "@/lib/api/queries/trainer";
 import { MyLesson } from "@/types/lesson";
 import { Quote } from "@/types/quote";
-import { LessonType } from "@/types/types";
 import ChipLessonType from "../Chip/ChipLessonType";
 import CardContainer from "../Common/Card/CardContainer";
 import TrainerInfo from "../Common/Card/TrainerInfo/TrainerInfo";
 import Loading from "../Common/Loading";
 
 export default function ActiveLessonCard({ item, quote }: { item: MyLesson; quote: Quote }) {
-  /**
-   * @TODO UI 변경해야함
-   */
-  const { data, isLoading, isError } = useQuery(
-    ["trainer-info", quote.trainerId],
-    () => getTrainerInfo(quote.trainerId),
-    {
-      enabled: !!quote.trainerId,
-    },
-  );
-
-  const { data: favorite } = useQuery(
-    ["favorite", quote.trainerId],
-    () => getFavorite(quote.trainerId),
-    {
-      enabled: !!quote.trainerId,
-    },
-  );
+  const { data: trainer, isLoading, isError } = useGetTrainer(quote?.trainerId);
+  const { data: favorite } = useGetFavoriteInfo(quote?.trainerId);
 
   if (isLoading) return <Loading />;
   if (isError) return toast.error("트레이너 정보를 불러오는 중 에러가 발생했어요! 😢");
 
-  const trainerInfo = data?.profile ?? {};
+  const trainerInfo = trainer?.profile ?? {};
 
   return (
     <CardContainer width="100%" gap="1.6rem">
       <div className="flex gap-[0.8rem] pc:gap-[1.2rem]">
-        <ChipLessonType lessonType={item.lessonType as LessonType} size="lg" />
+        <ChipLessonType lessonType={item.lessonType} size="lg" />
       </div>
       <TrainerInfo
         name={trainerInfo.name}
         rating={trainerInfo.rating}
         reviewCount={trainerInfo.reviewCount}
-        experience={trainerInfo.experience}
+        experience={trainerInfo.experience || 0}
         lessonCount={trainerInfo.lessonCount}
         isFavorited={favorite?.isFavorite}
         favoriteCount={favorite?.favoriteTotalCount}
