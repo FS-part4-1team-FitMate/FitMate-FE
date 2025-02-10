6a1aec5f219080a6f7f0449833cb24285c3382ab
Seo-Yurim <yurim2204@naver.com>
2025-02-10 13:25:11 +0900

    design: 내 레슨 관리 페이지들 empty UI 수정

--- src/components/ActiveLesson/ActiveEmpty.tsx
+++ src/components/ActiveLesson/ActiveEmpty.tsx
@@ -33,15 +33,14 @@ export default function ActiveEmpty() {
       <HorizontalLine width="100%" />
       <div className="flex flex-col gap-8 w-full text-md font-regular pc:text-lg">
         <div className="flex justify-between items-center">
-          <p>레슨 요청을 원하신다면?</p>
+          <p>아직 레슨 요청을 하지 않으셨다면?</p>
           <Button className="hover:text-blue-300 hover:bg-blue-100 hover:border hover:border-blue-300 px-6 rounded-3xl text-md text-white font-semibold bg-blue-300">
             <Link href="/user/create-request">레슨 요청하러 가기</Link>
           </Button>
         </div>
         <div className="flex justify-between items-center">
           <p>
-            요청한 레슨에
-            <br className="pc:hidden tablet:hidden mobile:block" />
+            요청한 레슨에 <br className="pc:hidden tablet:hidden mobile:block" />
             강사님을 직접 지정하고 싶다면?
           </p>
           <Button className="hover:text-blue-300 hover:bg-blue-100 hover:border hover:border-blue-300 px-6 rounded-3xl text-md text-white font-semibold bg-blue-300">
