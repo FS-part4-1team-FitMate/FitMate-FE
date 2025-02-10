f9c2c355182600cb8dec4529afeb83b492c32950
kipid <kipacti@gmail.com>
2025-02-11 08:08:49 +0900

    Chore: Optional with null :: components\Common\Card\TrainerInfo\Experience.tsx

--- src/components/Common/Card/TrainerInfo/Experience.tsx
+++ src/components/Common/Card/TrainerInfo/Experience.tsx
@@ -1,6 +1,6 @@
 const text = "font-medium pc:text-lg tablet:text-sm mobile:text-sm";
 
-export default function Experience({ experience }: { experience?: number }) {
+export default function Experience({ experience }: { experience?: number | null }) {
   return (
     <div className="flex items-center pc:gap-[0.6rem] tablet:gap-[0.4rem] mobile:gap-[0.4rem]">
       <p className={`${text} text-gray-300`}>경력</p>
