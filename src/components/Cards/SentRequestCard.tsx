ab11a09b1dae77ae07b9e7713696a77cdb82823e
mingmungXD <rex02033@gmail.com>
2025-02-12 09:09:22 +0900

    [천우승] Feat: 오류 수정

--- src/components/Cards/SentRequestCard.tsx
+++ src/components/Cards/SentRequestCard.tsx
@@ -7,6 +7,7 @@ import CardContainer from "../Common/Card/CardContainer";
 import LessonInfo from "../Common/Card/LessonInfo";
 import QuotePrice from "../Common/Card/QuotePrice";
 import { HorizontalLine } from "../Common/Line";
+import formatTime from "@/lib/utils/formatTime";
 
 /**
  *
@@ -18,7 +19,7 @@ export default function SentRequestCard({ item }: { item: any }) {
     <CardContainer width="100%" gap="1.6rem">
       <div className="flex justify-between">
         <ChipLessonType lessonType={item.lessonType as LessonType} size="lg" />
-        <p className="text-gray-500 text-xs font-normal">1시간 전</p>
+        <p className="text-gray-500 text-xs font-normal">{formatTime(item.createdAt)}</p>
       </div>
       <div className="flex flex-col gap-[1.8rem] py-[1.6rem] px-[1.8rem]">
         <p className="text-xl font-semibold">{item.name} 고객님</p>
--- src/pages/user/create-request.tsx
+++ src/pages/user/create-request.tsx
@@ -16,7 +16,7 @@ type FormValues = {
   lessonCount: number;
   lessonTime: number;
   locationType: string;
-  address?: string;
+  roadAddress?: string;
 };
 
 const createRequest = () => {
@@ -37,7 +37,7 @@ const createRequest = () => {
       lessonCount: 0,
       lessonTime: 0,
       locationType: "",
-      address: "",
+      roadAddress: "",
     },
   });
 
@@ -49,7 +49,7 @@ const createRequest = () => {
     "lessonCount",
     "lessonTime",
     "locationType",
-    "address",
+    "roadAddress",
   ];
 
   useEffect(() => {
@@ -377,25 +377,25 @@ const createRequest = () => {
         {step === 6 && (
           <div className="space-y-4">
             <Controller
-              name="address"
+              name="roadAddress"
               control={control}
               render={({ field }) => (
                 <input
                   {...field}
-                  id="address"
+                  id="roadAddress"
                   type="button"
                   className="w-full py-2 px-4 rounded-lg text-lg border"
-                  value={watch("address") || ""}
+                  value={watch("roadAddress") || ""}
                   onClick={() =>
                     new window.daum.Postcode({
                       oncomplete: function (data: any) {
                         var addr = "";
                         if (data.userSelectedType === "R") {
-                          addr = data.roadAddress;
+                          addr = data.roadaddress;
                         } else {
-                          addr = data.jibunAddress;
+                          addr = data.jibunaddress;
                         }
-                        setValue("address", addr);
+                        setValue("roadAddress", addr);
                       },
                     }).open()
                   }
@@ -405,7 +405,7 @@ const createRequest = () => {
             <button
               type="submit"
               className="w-full mt-4 bg-blue-300 text-white py-3 rounded-lg hover:bg-green-600"
-              disabled={!watch("address")}
+              disabled={!watch("roadAddress")}
             >
               견적 요청하기
             </button>
