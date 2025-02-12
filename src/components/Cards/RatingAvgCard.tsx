b0148bc4467645a89310bed388c614e1639806d4
Seo-Yurim <yurim2204@naver.com>
2025-02-12 09:20:36 +0900

    design: 리뷰 텍스트 사이즈 조정

--- src/components/Cards/RatingAvgCard.tsx
+++ src/components/Cards/RatingAvgCard.tsx
@@ -8,8 +8,10 @@ function RatingAvgCard({ ratingAvg }: Props) {
   return (
     <div className="flex flex-col justify-normal items-center gap-[16px]">
       <div className="flex items-baseline">
-        <span className="text-black-400 text-[6.4rem] font-bold">{ratingAvg.toFixed(1)}&nbsp;</span>
-        <span className="text-gray-100 text-[3.8rem] font-bold">/ 5</span>
+        <span className="text-black-400 text-[4rem] pc:text-[6.4rem] font-bold">
+          {ratingAvg.toFixed(1)}&nbsp;
+        </span>
+        <span className="text-gray-100 text-2xl pc:text-[3.8rem] font-bold">/ 5</span>
       </div>
       <StarPoints rating={ratingAvg / 5} r={16} r0={9} background={false} />
     </div>
--- src/components/Cards/RatingStatCard.tsx
+++ src/components/Cards/RatingStatCard.tsx
@@ -13,7 +13,7 @@ function RatingStatCard({ ratingStat }: Props) {
       <div className="text-lg font-medium rounded-2xl w-[330px] max-w-full p-[10px]">
         {ratingStatRev?.map((val, idx) => (
           <div key={idx} className="flex justify-between items-center py-[5px] px-[8px] gap-[10px]">
-            <div className="text-black-300 text-lg font-medium text-left w-[30px] shrink-0 pc:text-xl">
+            <div className="text-black-300 text-md font-medium text-left w-[30px] shrink-0 pc:text-xl">
               {val.rating}점
             </div>
             <div className="relative bg-bg-300 grow-1 shrink-1 w-[250px] h-[10px] rounded-full">
@@ -22,7 +22,7 @@ function RatingStatCard({ ratingStat }: Props) {
                 style={{ width: `${(val.count / sum) * 100}%` }}
               ></div>
             </div>
-            <div className="text-gray-300 text-lg font-bold text-left w-[30px] shrink-0 pc:text-xl">
+            <div className="text-gray-300 text-md font-bold text-left w-[30px] shrink-0 pc:text-xl">
               {val.count}
             </div>
           </div>
