7f05cf234de9a8ab76771773960620bc4cf3b847
kipid <kipacti@gmail.com>
2025-02-12 14:32:53 +0900

    Feat: readNoti :: lib\api\notiService.ts

--- src/lib/api/notiService.ts
+++ src/lib/api/notiService.ts
@@ -1,7 +1,12 @@
 import { NotiParams } from "@/types/notis";
-import { get } from "./method";
+import { get, patch } from "./method";
 
 export async function getNotiList(params: NotiParams) {
   const res = await get("/notifications", params);
+  return res.data;
+}
+
+export async function readNoti(notiId: number) {
+  const res = await patch(`/notifications/${notiId}/read`);
   return res.data;
 }
