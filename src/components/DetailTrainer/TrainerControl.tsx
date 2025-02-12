9fa78e5a0d4a0197304c3e736aaab96aea76b77d
Seo-Yurim <yurim2204@naver.com>
2025-02-12 09:59:18 +0900

    design: 페이지네이션 + 강사상세 컨트롤 컴포넌트 반응형 수정

--- src/components/DetailTrainer/TrainerControl.tsx
+++ src/components/DetailTrainer/TrainerControl.tsx
@@ -29,20 +29,20 @@ export default function TrainerControl({ profile }: { profile: Profile }) {
   };
 
   return (
-    <div className="flex flex-col gap-4 pc:gap-[3.2rem]">
+    <div className="relative flex flex-col gap-4 pc:gap-[3.2rem]">
       <h1 className="hidden text-xl font-semibold pc:block">
         {profile?.name} 강사님에게 지정 견적을 요청해보세요
       </h1>
-      <div className="flex flex-row gap-[0.8rem] w-full p-4 pc:flex-col pc:gap-[3.2rem] pc:px-0">
-        <Button className="hover:bg-red-100 hover:border hover:border-red-200 hover:text-red-200 h-[5.4rem] p-4 rounded-[1.6rem] font-semibold pc:w-[35.3rem] pc:text-xl hidden gap-4 border border-line-200 bg-gray-50 pc:flex">
+      <div className="fixed left-1/2 transform -translate-x-1/2 bottom-4 flex flex-row gap-[0.8rem] max-w-[74.4rem] w-full px-8 pc:relative pc:flex-col pc:gap-[3.2rem] pc:px-0">
+        <Button className="hover:bg-red-100 hover:border hover:border-red-200 hover:text-red-200 h-[5.4rem] p-4 rounded-[1.6rem] font-semibold pc:w-[35.3rem] pc:text-xl hidden gap-4 border border-line-200 bg-gray-50 shadow-card pc:flex">
           <Favorite trainerId={trainerId as string} noneCount={true} /> 강사님 찜하기
         </Button>
-        <div className="flex justify-center items-center w-[5.4rem] h-[5.4rem] p-4 border border-line-200 rounded-[1.6rem] pc:hidden">
+        <div className="hover:bg-red-100 hover:border hover:border-red-200 hover:text-red-200 flex justify-center items-center w-[5.4rem] h-[5.4rem] p-4 border border-line-200 rounded-[1.6rem] bg-white pc:hidden">
           <Favorite trainerId={trainerId as string} noneCount={true} />
         </div>
         <Button
           onClick={handleLessonRequest}
-          className="hover:bg-blue-100 hover:border hover:border-blue-300 hover:text-blue-300 h-[5.4rem] p-4 rounded-[1.6rem] font-semibold w-full text-gray-50 bg-blue-300 pc:w-[35.3rem] pc:text-xl"
+          className="hover:bg-blue-100 hover:border hover:border-blue-300 hover:text-blue-300 h-[5.4rem] p-4 rounded-[1.6rem] font-semibold w-full text-gray-50 bg-blue-300 shadow-card pc:w-[35.3rem] pc:text-xl"
         >
           지정 견적 요청하기
         </Button>
