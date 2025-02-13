1c7f1b740387ff47b3823df54777102fa33145fa
kipid <kipacti@gmail.com>
2025-02-13 14:15:58 +0900

    Feat: getServerSideProps 사용, sns-login fail handled

--- src/pages/sns-login.tsx
+++ src/pages/sns-login.tsx
@@ -1,51 +1,92 @@
 import { useSetUser } from "@/contexts/UserProvider";
+import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import toast from "react-hot-toast";
 import { Role } from "@/types/types";
 import Loading from "@/components/Common/Loading";
 
-function SNSLogIn() {
+interface QueryParams {
+  accessToken?: string;
+  refreshToken?: string;
+  user?: string;
+  hasProfile?: string;
+  message?: string;
+}
+
+interface PageProps {
+  initialQuery: QueryParams;
+}
+
+// 초기 로딩시 router.query 도 빈 JSON 객체이고, useSearchParams() 도 빈 JSON 객체이기 때문에 getServerSideProps 에서 초기값을 설정해 주어야 함.
+export const getServerSideProps: GetServerSideProps<PageProps> = async (context) => {
+  const {
+    accessToken = "",
+    refreshToken = "",
+    user = "",
+    hasProfile = "",
+    message = "",
+  } = context.query;
+
+  return {
+    props: {
+      initialQuery: {
+        accessToken: accessToken as string,
+        refreshToken: refreshToken as string,
+        user: user as string,
+        hasProfile: hasProfile as string,
+        message: message as string,
+      },
+    },
+  };
+};
+
+function SNSLogIn({ initialQuery }: PageProps) {
   const router = useRouter();
   const setUser = useSetUser();
 
-  useEffect(() => {
-    const query = new URLSearchParams(window.location.search);
-    const accessToken = query.get("accessToken");
-    const refreshToken = query.get("refreshToken");
-    const user = JSON.parse(query.get("user")!);
-    const hasProfileString = query.get("hasProfile") as string;
-    let hasProfile = false;
-    if (hasProfileString === "true") {
-      hasProfile = true;
-    }
+  const message = initialQuery?.message;
+  if (message) {
+    toast.error(message);
+  }
+  const accessToken = initialQuery?.accessToken;
+  const refreshToken = initialQuery?.refreshToken;
+  const user = JSON.parse(initialQuery?.user!);
+  const hasProfileString = initialQuery?.hasProfile;
+  let hasProfile = false;
+  if (hasProfileString === "true") {
+    hasProfile = true;
+  }
 
-    if (accessToken && refreshToken && user) {
-      localStorage.setItem(
-        "userData",
-        JSON.stringify({ accessToken, refreshToken, user, hasProfile }),
-      );
+  if (accessToken && refreshToken && user) {
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({ accessToken, refreshToken, user, hasProfile }),
+    );
 
-      // 사용자 정보 업데이트
-      setUser({ ...user, hasProfile });
+    // 사용자 정보 업데이트
+    setUser({ ...user, hasProfile });
+    toast.success("로그인에 성공하였습니다.");
 
-      // 권한에 따라 페이지 이동
-      if (user.role === Role.USER) {
-        if (user.hasProfile) {
-          router.replace("/user/my-lesson/active-lesson");
-        } else {
-          router.replace("/user/profile/regist");
-        }
-      } else if (user.role === Role.TRAINER) {
-        if (user.hasProfile) {
-          router.replace("/trainer/received-request");
-        } else {
-          router.replace(`/trainer/${user.id}/profile/regist`);
-        }
+    // 권한에 따라 페이지 이동
+    if (user.role === Role.USER) {
+      if (user.hasProfile) {
+        router.replace("/user/my-lesson/active-lesson");
+      } else {
+        router.replace("/user/profile/regist");
       }
-    } else {
-      router.replace("/login");
+    } else if (user.role === Role.TRAINER) {
+      if (user.hasProfile) {
+        router.replace("/trainer/received-request");
+      } else {
+        router.replace(`/trainer/${user.id}/profile/regist`);
+      }
     }
-  }, [router, setUser]);
+  } else {
+    setTimeout(() => {
+      router.replace("/login");
+    }, 3000);
+  }
 
   return <Loading />;
 }
