7cb00c40f1165b8626a4ccfd8eba2891e87130b3
Seo-Yurim <yurim2204@naver.com>
2025-02-13 10:33:12 +0900

    feat: 칩 추가

--- src/components/Cards/QuoteCard.tsx
+++ src/components/Cards/QuoteCard.tsx
@@ -3,7 +3,9 @@ import { useGetFavoriteInfo, useGetTrainer } from "@/lib/api/queries/trainer";
 import formatPrice from "@/lib/utils/formatPrice";
 import { Lesson } from "@/types/lesson";
 import { Quote } from "@/types/quote";
+import { LessonRequestStatus } from "@/types/types";
 import ChipLessonType from "../Chip/ChipLessonType";
+import ChipRequestStatus from "../Chip/ChipRequestStatus";
 import CardContainer from "../Common/Card/CardContainer";
 import QuotePrice from "../Common/Card/QuotePrice";
 import TrainerInfo from "../Common/Card/TrainerInfo/TrainerInfo";
@@ -22,6 +24,9 @@ export default function QuoteCard({ myLesson, quote }: { myLesson: Lesson; quote
     <CardContainer width="100%" gap="1.6rem">
       <div className="flex gap-[0.8rem] pc:gap-[1.2rem]">
         <ChipLessonType lessonType={myLesson?.lessonType} />
+        {quote.status === "ACCEPTED" && (
+          <ChipRequestStatus requestStatus={LessonRequestStatus.COMPLETED} />
+        )}
       </div>
       <p className="text-black-300 text-md font-semibold pc:text-2xl">{trainerInfo?.intro}</p>
       <TrainerInfo
