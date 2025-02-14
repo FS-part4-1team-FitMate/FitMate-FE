4780295a3d2b1bd056cf631a19d024895f01cd7f
mingmungXD <rex02033@gmail.com>
2025-02-15 04:45:21 +0900

    자잘한 수정

--- src/components/Cards/SentRequestCard.tsx
+++ src/components/Cards/SentRequestCard.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import formatDate from "@/lib/utils/formatDate";
 import formatPrice from "@/lib/utils/formatPrice";
-import formatTime from "@/lib/utils/formatTime";
 import { LessonType, LocationType, locationType_trans } from "@/types/types";
 import ChipLessonType from "../Chip/ChipLessonType";
 import CardContainer from "../Common/Card/CardContainer";
@@ -15,7 +14,7 @@ import { Quote } from "@/types/quote";
  * @TODO replace any
  */
 
-export default function QuoteDetailCard({ item }: { item: Quote }) {
+export default function SentRequestCard({ item }: { item: Quote }) {
   const lessonRequest = item.lessonRequest;
   return (
     <CardContainer width="100%" gap="1.6rem">
--- src/pages/trainer/managing-request/sent-request/[id].tsx
+++ src/pages/trainer/managing-request/sent-request/[id].tsx
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 import { useQuery } from "@tanstack/react-query";
 import { getDetailRequest } from "@/lib/api/requestService";
-import SentRequestCard from "@/components/Cards/SentRequestCard";
 import { HorizontalLine } from "@/components/Common/Line";
 import LessonInfo from "@/components/Common/LessonInfo";
 import ShareSNS from "@/components/Common/ShareSNS";
@@ -14,11 +13,11 @@ export default function DetailRequest() {
   const [lessonRequestId, setLessonRequestId] = useState<string | null>(null);
 
   useEffect(() => {
-    console.log("Router Query:", router.query); // router.query가 어떤 값을 가지고 있는지 확인
-    console.log("Router isReady:", router.isReady); // router.isReady가 언제 true가 되는지 확인
+    console.log("Router Query:", router.query);
+    console.log("Router isReady:", router.isReady);
   
     if (router.isReady) {
-      console.log("Router Query Keys:", Object.keys(router.query)); // query에 어떤 키들이 있는지 확인
+      console.log("Router Query Keys:", Object.keys(router.query));
       setLessonRequestId(router.query.id as string);
       console.log("Updated lessonRequestId:", router.query.id);
     }
@@ -30,7 +29,7 @@ export default function DetailRequest() {
       console.log("API 요청 실행됨:", lessonRequestId);
       return getDetailRequest(lessonRequestId as string);
     },
-    enabled: !!lessonRequestId, // lessonRequestId가 있을 때만 실행
+    enabled: !!lessonRequestId,
   });
 
 
