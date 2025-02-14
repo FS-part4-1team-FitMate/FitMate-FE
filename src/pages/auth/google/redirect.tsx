e208747912ef2c0d28a3ea970b1e3d7b91d920ee
kipid <kipacti@gmail.com>
2025-02-14 13:48:46 +0900

    Delete auth/../redirect

--- src/pages/auth/google/redirect.tsx
+++
@@ -1,9 +0,0 @@
-function AuthGoogleRedirect() {
-  return (
-    <div className="text-2xl text-center my-[40px]">
-      구글 로그인에 실패 했습니다. 아마도 계정이 없으신것 같습니다. 가입 후 로그인 해주세요.
-    </div>
-  );
-}
-
-export default AuthGoogleRedirect;
