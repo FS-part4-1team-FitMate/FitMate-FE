7702b0d9f688f02d13dad1d1da43c755b26d154e
kipid <kipacti@gmail.com>
2025-02-15 20:17:03 +0900

    Update SentRequestCard.tsx

--- src/components/Cards/SentRequestCard.tsx
+++ src/components/Cards/SentRequestCard.tsx
@@ -2,40 +2,41 @@ import Link from "next/link";
 import formatDate from "@/lib/utils/formatDate";
 import formatPrice from "@/lib/utils/formatPrice";
 import formatTime from "@/lib/utils/formatTime";
+import { Quote } from "@/types/quote";
 import { LessonType, LocationType, locationType_trans } from "@/types/types";
 import ChipLessonType from "../Chip/ChipLessonType";
 import CardContainer from "../Common/Card/CardContainer";
 import LessonInfo from "../Common/Card/LessonInfo";
 import QuotePrice from "../Common/Card/QuotePrice";
 import { HorizontalLine } from "../Common/Line";
-import { Quote } from "@/types/quote";
 
 /**
  *
  * @TODO replace any
  */
 
-export default function QuoteDetailCard({ item }: { item: Quote }) {
+export default function SentRequestCard({ item }: { item: Quote }) {
   const lessonRequest = item.lessonRequest;
   return (
     <CardContainer width="100%" gap="1.6rem">
       <Link href={`/trainer/managing-request/sent-request/${item.lessonRequestId}`}>
-      <div className="flex justify-between">
+        <div className="flex justify-between">
           <ChipLessonType lessonType={lessonRequest?.lessonType as LessonType} />
-        <p className="text-gray-500 text-xs font-normal">{formatTime(item.createdAt)}</p>
-      </div>
-      <div className="flex flex-col gap-[1.8rem] py-[1.6rem] px-[1.8rem]">
-        <p className="text-xl font-semibold">{lessonRequest?.userId} 고객님</p>
-        <HorizontalLine width="100%" />
+          <p className="text-gray-500 text-xs font-normal">{formatTime(item.createdAt)}</p>
+        </div>
+        <div className="flex flex-col gap-[1.8rem] py-[1.6rem] px-[1.8rem]">
+          <p className="text-xl font-semibold">{lessonRequest?.userId} 고객님</p>
+          <HorizontalLine width="100%" />
           <LessonInfo
-            startDate={formatDate(lessonRequest?.startDate)}
-            endDate={formatDate(lessonRequest?.endDate)}
+            startDate={
+              lessonRequest?.startDate ? formatDate(lessonRequest?.startDate) : "날짜 없음"
+            }
+            endDate={lessonRequest?.endDate ? formatDate(lessonRequest?.endDate) : "날짜 없음"}
             locationType={locationType_trans[lessonRequest?.locationType as LocationType]}
             address={lessonRequest?.roadAddress || "주소 없음"}
           />
-
-      </div>
-      <QuotePrice price={formatPrice(item.price)} />
+        </div>
+        <QuotePrice price={formatPrice(item.price)} />
       </Link>
     </CardContainer>
   );
