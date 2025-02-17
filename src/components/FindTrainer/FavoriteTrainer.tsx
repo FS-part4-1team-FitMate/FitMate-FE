cd787bc437c7d9790fd2c216230039b3bbaddb50
Seo-Yurim <yurim2204@naver.com>
2025-02-17 20:39:53 +0900

    fix: 오류

--- src/components/FindTrainer/FavoriteTrainer.tsx
+++ src/components/FindTrainer/FavoriteTrainer.tsx
@@ -15,11 +15,9 @@ export default function FavoriteTrainer() {
     setUserId(user?.id ?? null);
   }, [user]);
 
-  const {
-    data: trainerList = [],
-    isLoading,
-    isError,
-  } = useGetFavoriteTrainer(userId || "", { page: 1, limit: 100 });
+  const { data, isLoading, isError } = useGetFavoriteTrainer(userId || "", { page: 1, limit: 3 });
+
+  const trainerList = data?.trainers ?? [];
 
   if (isLoading && userId) return <Loading />;
   if (isError) return toast.error("찜한 강사님 목록을 불러오는 중 에러가 발생했어요! 😢");
@@ -27,25 +25,20 @@ export default function FavoriteTrainer() {
   return (
     <div className={clsx(!userId ? "hidden" : "flex flex-col gap-[1.6rem] w-[32.7rem]")}>
       <p className="text-xl font-semibold">찜한 강사님</p>
-      {trainerList
-        .slice(0, 3)
-        .map(
-          (trainer: Trainer) =>
-            trainer?.isFavorite === true && (
-              <FavoriteTrainerCard
-                key={trainer.id}
-                name={trainer.nickname}
-                rating={trainer.profile.rating}
-                reviewCount={trainer.profile.reviewCount}
-                experience={trainer.profile.experience}
-                lessonCount={trainer.profile.lessonCount}
-                isFavorited={trainer.isFavorite}
-                favoriteCount={trainer._count.favoritedByUsers}
-                lessonType={trainer.profile.lessonType}
-                size="sm"
-              />
-            ),
-        )}
+      {trainerList.map((trainer: Trainer) => (
+        <FavoriteTrainerCard
+          key={trainer.id}
+          name={trainer.nickname}
+          rating={trainer.profile.rating}
+          reviewCount={trainer.profile.reviewCount}
+          experience={trainer.profile.experience}
+          lessonCount={trainer.profile.lessonCount}
+          isFavorited={true}
+          favoriteCount={trainer._count.favoritedByUsers}
+          lessonType={trainer.profile.lessonType}
+          size="sm"
+        />
+      ))}
     </div>
   );
 }
