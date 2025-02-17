594390d8d1885892a09b010a756e02e61f51e9ff
Seo-Yurim <yurim2204@naver.com>
2025-02-17 20:37:09 +0900

    feat: 랜딩 초안

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,81 +1,185 @@
-import { img_landing_01, img_landing_02, img_landing_03 } from "@/imageExports";
+import { ic_fitness, ic_rehab, ic_sports } from "@/imageExports";
 import clsx from "clsx";
 import Image from "next/image";
-import Link from "next/link";
+import { useEffect, useRef } from "react";
 
-const service_container = clsx(
-  "flex flex-col gap-[4.4rem] w-[32.7rem]",
-  " pc:gap-[2.4rem] pc:flex-row pc:w-[122rem] pc:h-[59.8rem]",
+const card_title = clsx(
+  "p-1 border border-blue-300 rounded-full",
+  "text-blue-300 text-lg font-semibold bg-blue-100",
+  "pc:text-2xl tablet:text-xl",
 );
-const img_card = clsx(
-  "relative flex flex-col justify-start items-start w-full h-[24rem]",
-  "py-[4rem] px-[4.2rem] rounded-[3.2rem] shadow-card bg-blue-100",
-);
-const button = clsx(
-  "flex-1 w-full h-[5.4rem] p-[1.6rem] rounded-[5rem] text-lg font-semibold",
-  "pc:w-[34rem] pc:h-[6.4rem] pc:text-xl",
-);
+const card_description = "text-gray-400 text-sm font-normal pc:text-2lg tablet:text-2lg";
+const card_img =
+  "border border-blue-300 rounded-[1.6rem] p-4 w-[10rem] h-[10rem] pc:w-[25rem] pc:h-[25rem] tablet:w-[20rem] tablet:h-[20rem]";
 
 export default function Home() {
+  const stickyRef = useRef<HTMLDivElement>(null);
+  const mainContentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    class CardFlipOnScroll {
+      wrapper: HTMLElement;
+      sticky: HTMLElement;
+      cards: NodeListOf<HTMLElement>;
+      length: number;
+      start: number;
+      end: number;
+      step: number;
+
+      constructor(wrapper: HTMLElement, sticky: HTMLElement) {
+        this.wrapper = wrapper;
+        this.sticky = sticky;
+        this.cards = sticky.querySelectorAll(".card");
+        this.length = this.cards.length;
+
+        this.start = 0;
+        this.end = 0;
+        this.step = 0;
+      }
+
+      init() {
+        if (!this.wrapper || !this.sticky) return;
+        this.start = this.wrapper.offsetTop - 100;
+        this.end = this.wrapper.offsetTop + this.wrapper.offsetHeight - window.innerHeight * 1.2;
+        this.step = (this.end - this.start) / (this.length * 2);
+      }
+
+      animate() {
+        this.cards.forEach((card, i) => {
+          const s = this.start + this.step * i;
+          const e = s + this.step * (this.length + 1);
+
+          if (window.scrollY <= s) {
+            card.style.transform = `
+              perspective(100vw)
+              translateX(100vw) 
+              rotateY(180deg)
+            `;
+          } else if (window.scrollY > s && window.scrollY <= e - this.step) {
+            card.style.transform = `
+              perspective(100vw)
+              translateX(${100 + ((window.scrollY - s) / (e - s)) * -100}vw)
+              rotateY(180deg)
+            `;
+          } else if (window.scrollY > e - this.step && window.scrollY <= e) {
+            card.style.transform = `
+              perspective(100vw)
+              translateX(${100 + ((window.scrollY - s) / (e - s)) * -100}vw)
+              rotateY(${180 + (-(window.scrollY - (e - this.step)) / this.step) * 180}deg)
+            `;
+          } else if (window.scrollY > e) {
+            card.style.transform = `
+              perspective(100vw)
+              translateX(0vw) 
+              rotateY(0deg)
+            `;
+          }
+        });
+      }
+    }
+
+    const cardFlipOnScroll = new CardFlipOnScroll(mainContentRef.current!, stickyRef.current!);
+    cardFlipOnScroll.init();
+
+    const handleScroll = () => {
+      cardFlipOnScroll.animate();
+    };
+
+    const handleResize = () => {
+      cardFlipOnScroll.init();
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="flex flex-col items-center gap-[4.8rem] h-screen m-auto py-[8rem] px-8 bg-bg-200">
-      <h1 className="text-nowrap text-2xl font-semibold text-center pc:text-3lx">
-        원하는 운동 유형을 확인하고
-        <br />
-        레슨을 받아보세요
-      </h1>
-      <div className={service_container}>
-        <div className={`${img_card} bg-blue-100 pc:w-[43.2rem] pc:h-full`}>
-          <p className="text-xl font-semibold pc:text-2.5xl">재활운동</p>
-          <p className="text-gray-400 text-md font-normal pc:text-xl">스트레칭, 재활치료</p>
-          <Image
-            className="absolute right-8 bottom-0 w-[17rem] h-[17rem] pc:w-[34.8rem] pc:h-[34.8rem]"
-            src={img_landing_01}
-            width={348}
-            height={348}
-            alt="재활운동 이미지"
-            priority
-          />
-        </div>
-        <div className="flex flex-col gap-[4.4rem] pc:gap-[2.4rem]">
-          <div className={`${img_card} bg-gray-50 pc:flex-1 pc:w-[76.4rem]`}>
-            <p className="text-xl font-semibold pc:text-2.5xl">스포츠</p>
-            <p className="text-gray-400 text-md font-normal pc:text-xl">
-              축구, 테니스, 스키, 복싱, 주짓수 등
-            </p>
-            <Image
-              className="absolute right-0 bottom-0 w-[32.5rem] h-[11.5rem] pc:w-[42.5rem] pc:h-[21.5rem]"
-              src={img_landing_02}
-              width={425}
-              height={215}
-              alt="스포츠 이미지"
-              priority
-            />
+    <>
+      <div className="w-full h-[500vh]" ref={mainContentRef}>
+        <div
+          className="sticky top-0 flex justify-center items-center h-screen overflow-hidden"
+          ref={stickyRef}
+        >
+          <div className="flex flex-col justify-center items-center gap-8 py-[5rem] pc:py-[10rem] text-center pc:text-6xl">
+            <h1 className="animate-fade py-4 px-8 pc:py-8 pc:px-16 border border-blue-300 rounded-full bg-blue-100  text-blue-300 text-2lg font-semibold pc:text-3xl">
+              원하는 운동 유형을 확인하고
+              <br />
+              레슨을 받아보세요
+            </h1>
+            <h1 className="animate-bounce text-blue-300 text-2lg pc:text-3xl">▼</h1>
           </div>
-          <div className={`${img_card} bg-gray-50 pc:flex-1 pc:w-[76.4rem]`}>
-            <p className="text-xl font-semibold pc:text-2.5xl">피트니스</p>
-            <p className="text-gray-400 text-md font-normal pc:text-xl">
-              PT, 요가, 필라테스, 식단 관리
-            </p>
-            <Image
-              className="absolute -right-10 bottom-0 w-[21.2rem] h-[14rem] pc:right-0 pc:w-[31.2rem] pc:h-[27.3rem]"
-              src={img_landing_03}
-              width={312}
-              height={273}
-              alt="피트니스 이미지"
-              priority
-            />
+          <div className="card-frame">
+            <div className="card shadow-card">
+              <div className="front gap-4 p-4 pc:gap-8 pc:p-12">
+                <div className="flex flex-col gap-3">
+                  <h1 className={card_title}>재활운동</h1>
+                  <p className={card_description}>스트레칭, 재활치료</p>
+                </div>
+                <Image
+                  className={card_img}
+                  src={ic_rehab}
+                  width={250}
+                  height={250}
+                  alt="재활운동 이미지"
+                />
+              </div>
+              <div className="back"></div>
+            </div>
+            <div className="card shadow-card">
+              <div className="front gap-4 p-4 pc:gap-8 pc:p-12">
+                <div className="flex flex-col gap-3">
+                  <h1 className={card_title}>스포츠</h1>
+                  <p className={card_description}>
+                    축구, 테니스, 스키,
+                    <br className="block pc:hidden" /> 복싱, 주짓수 등
+                  </p>
+                </div>
+                <Image
+                  className={card_img}
+                  src={ic_sports}
+                  width={250}
+                  height={250}
+                  alt="스포츠 이미지"
+                />
+              </div>
+              <div className="back"></div>
+            </div>
+            <div className="card shadow-card">
+              <div className="front gap-4 p-4 pc:gap-8 pc:p-12">
+                <div className="flex flex-col gap-3">
+                  <h1 className={card_title}>피트니스</h1>
+                  <p className={card_description}>
+                    PT, 요가, 필라테스,
+                    <br className="block pc:hidden" /> 식단 관리
+                  </p>
+                </div>
+                <Image
+                  className={card_img}
+                  src={ic_fitness}
+                  width={250}
+                  height={250}
+                  alt="피트니스 이미지"
+                />
+              </div>
+              <div className="back"></div>
+            </div>
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-[1.1rem] max-w-[32.7rem] w-full pc:flex-row pc:max-w-fit">
-        <button className={`${button} text-gray-50 bg-blue-300`}>
-          <Link href="/login">로그인</Link>
-        </button>
-        <button className={`${button} border border-blue-300 text-blue-300 bg-gray-50`}>
-          <Link href="/user/signup">회원가입</Link>
-        </button>
+      <div className="bg-bg-200 flex flex-col justify-center items-center w-full h-screen text-center text-5xl">
+        <h1 className="animate-bounce">
+          맞춤형 트레이닝 서비스,
+          <br />
+          지금 바로 시작해보세요!
+        </h1>
+        <button>로그인</button>
+        <button>회원가입</button>
       </div>
-    </div>
+    </>
   );
 }
