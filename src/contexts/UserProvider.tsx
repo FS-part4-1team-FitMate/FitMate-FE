0cce7ef4ef371f62fb513d86d446080029b14acb
kipid <kipacti@gmail.com>
2025-02-19 13:37:20 +0900

    Update UserProvider.tsx

--- src/contexts/UserProvider.tsx
+++ src/contexts/UserProvider.tsx
@@ -50,7 +50,7 @@ export function UserProvider({ children }: Props) {
         }
       }
     }
-  }, []);
+  }, [router.pathname]);
 
   return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
 }
