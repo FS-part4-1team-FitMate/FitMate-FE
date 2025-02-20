eb291558910b6b925c60abc63a90075ccb367b63
kipid <kipacti@gmail.com>
2025-02-20 13:34:07 +0900

    Fix: axios.put to axios.patch in profile/edit

--- src/pages/trainer/[trainerId]/profile/edit.tsx
+++ src/pages/trainer/[trainerId]/profile/edit.tsx
@@ -123,10 +123,13 @@ function ProfileEdit() {
       delete changedData.updatedAt;
       const userProfile = await patchProfile(user?.id!, changedData);
       if (userProfile && "profileImagePresignedUrl" in userProfile) {
-        await axios.put(userProfile.profileImagePresignedUrl as string, profileImageFileToUpload);
+        await axios.patch(userProfile.profileImagePresignedUrl as string, profileImageFileToUpload);
       }
       if (userProfile && "certificationPresignedUrl" in userProfile) {
-        await axios.put(userProfile.certificationPresignedUrl as string, certificationFileToUpload);
+        await axios.patch(
+          userProfile.certificationPresignedUrl as string,
+          certificationFileToUpload,
+        );
       }
       try {
         const userDataLS = JSON.parse(localStorage.getItem("userData")!);
